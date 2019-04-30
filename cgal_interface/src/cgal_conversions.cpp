da773a8b8c785dc22c94cb29c8d008e4e138f813
johnny <abel.gawel@mavt.ethz.ch>
2019-04-30 10:34:12 +0200

    wip.

--- cgal_interface/src/cgal_conversions.cpp
+++ cgal_interface/src/cgal_conversions.cpp
@@ -5,9 +5,8 @@ namespace cgal {
 
 void meshToPointCloud(const SurfaceMesh &mesh,
                       PointCloud *pc) {
-//  pc->header
   pc->width = mesh.size_of_vertices();
-  pc->header.frame_id = "mesh"
+  pc->header.frame_id = "mesh";
   pcl::PointXYZ point;
   for (auto vertex = mesh.points_begin();
        vertex != mesh.points_end(); ++vertex) {
@@ -21,55 +20,42 @@ void meshToPointCloud(const SurfaceMesh &mesh,
 void meshToROSMsg(const SurfaceMesh &mesh,
                       cgal_msgs::ProbabilisticMesh *msg) {
 
-//  PointCloud pc;
-//  pc.width = P_.size_of_vertices();
-//  pcl::PointXYZ point;
-//  for (auto vertex = P_.points_begin();
-//       vertex != P_.points_end(); ++vertex) {
-//    point.x = vertex->x();
-//    point.y = vertex->y();
-//    point.z = vertex->z();
-//    pc.push_back(point);
-//  }
-//  return pc;
+// Transfer points to msg.
+  geometry_msgs::Point point;
+  for (auto vertex = mesh.points_begin();
+       vertex != mesh.points_end(); ++vertex) {
+    point.x = vertex->x();
+    point.y = vertex->y();
+    point.z = vertex->z();
+    msg->vertices.push_back(point);
+  }
+  // Transfer mesh faces to msg.
+  shape_msgs::MeshTriangle triangle;
+  for (auto facet = mesh.facets_begin();
+       facet != mesh.facets_end(); ++facet) { // all mesh surfaces
 
+    if (facet->is_triangle()) {
+      triangle.vertex_indices[0] = facet->halfedge()->vertex()->info();
+      triangle.vertex_indices[1] = facet->halfedge()->next()->vertex()->info();
+      triangle.vertex_indices[2] = facet->halfedge()->opposite()->vertex()
+          ->info();
 
-//  std::vector<Kernel::Point_3> points;
-//  points.resize(input->size());
-//  for (size_t i = 0; i < input->size(); ++i) {
+//      for (int i = 0; i < 3; ++i) {
+////        Triangle t;
+////        t.triangle(facet);
+//        triangle.vertex_indices[i] = facet->halfedge()->vertex()->info();
 //
-//    if (input->at(i).z < 0.2) {
-//      continue;
-//    }
-//
-//    points[i] = Kernel::Point_3(input->at(i).x, input->at(i).y, input->at(i).z);
-//    vh = dt.insert(points[i]);
-//    vh->info() = used_points++;
-//
-//    geometry_msgs::Point current_point;
-//    current_point.x = input->at(i).x;
-//    current_point.y = input->at(i).y;
-//    current_point.z = input->at(i).z;
-//
-//    msg->vertices.push_back(current_point);
-//    msg->cov_vertices.push_back(geometry_msgs::Point());
-//  }
-//
-//  for (Delaunay::Cell_iterator cit = dt.cells_begin(); cit != dt.cells_end(); ++cit) {
-//    shape_msgs::MeshTriangle triangle;
-//    bool weird = false;
-//
-//    for (int i = 0; i < 3; ++i) {
-//      triangle.vertex_indices[i] = cell->vertex(i + 1)->info();
-//
-//      if (triangle.vertex_indices[i] > used_points) {
-//        weird = true;
+////      if (triangle.vertex_indices[i] > used_points) {
+////        weird = true;
+////      }
 //      }
-//    }
-//
+
 //    if (!weird) {
-//      msg->triangles.push_back(triangle);
+      msg->triangles.push_back(triangle);
 //    }
+    }
+  }
+
 
 }
 }
