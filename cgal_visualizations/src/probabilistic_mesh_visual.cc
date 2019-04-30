b7e2a80ff35df1fdf24839024f04d856f0ac18e7
Hermann <blumh@ethz.ch>
2019-04-30 15:44:38 +0200

    small comments and style fixes

--- cgal_visualizations/src/probabilistic_mesh_visual.cc
+++ cgal_visualizations/src/probabilistic_mesh_visual.cc
@@ -1,14 +1,14 @@
 #include <cgal_visualizations/probabilistic_mesh_visual.h>
 
-#include <limits>
 #include <chrono>
+#include <limits>
 
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreSceneNode.h>
+#include <ros/package.h>  // This dependency should be moved out of here, it is just used for a search path.
+#include <ros/ros.h>
 #include <rviz/ogre_helpers/shape.h>
 #include <rviz/properties/bool_property.h>
-#include <ros/package.h> // This dependency should be moved out of here, it is just used for a search path.
-#include <ros/ros.h>
 
 namespace cad_percept {
 
@@ -16,12 +16,12 @@ namespace visualizations {
 
 unsigned int ProbabilisticMeshVisual::instance_counter_ = 0;
 
-ProbabilisticMeshVisual::ProbabilisticMeshVisual(Ogre::SceneManager* scene_manager,
-                                                 Ogre::SceneNode* parent_node) :
-    visualize_covariances_(false),
-    backface_culling_(false),
-    edge_color_(0.0, 0.0, 0.0, 0.8),
-    surface_color_(0.0, 0.0, 1.0, 0.8) {
+ProbabilisticMeshVisual::ProbabilisticMeshVisual(
+    Ogre::SceneManager* scene_manager, Ogre::SceneNode* parent_node)
+    : visualize_covariances_(false),
+      backface_culling_(false),
+      edge_color_(0.0, 0.0, 0.0, 0.8),
+      surface_color_(0.0, 0.0, 1.0, 0.8) {
   scene_manager_ = scene_manager;
   frame_node_ = parent_node;
   instance_number_ = instance_counter_++;
@@ -30,28 +30,23 @@ ProbabilisticMeshVisual::ProbabilisticMeshVisual(Ogre::SceneManager* scene_manag
 
   using namespace std::chrono;
   unsigned long long ticks =
-      duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
+      duration_cast<milliseconds>(steady_clock::now().time_since_epoch())
+          .count();
   object_name_ = std::to_string(ticks);
 }
 
 void ProbabilisticMeshVisual::initResourcePaths() {
-
   // set up resource paths
   std::string rviz_path = ros::package::getPath("rviz");
   std::cout << rviz_path + "/ogre_media/models" << std::endl;
   std::cout << ROS_PACKAGE_NAME << std::endl;
 
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-      rviz_path + "/ogre_media/models",
-      "FileSystem",
-      ROS_PACKAGE_NAME);
+      rviz_path + "/ogre_media/models", "FileSystem", ROS_PACKAGE_NAME);
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-      rviz_path + "/ogre_media/materials",
-      "FileSystem",
-      ROS_PACKAGE_NAME);
+      rviz_path + "/ogre_media/materials", "FileSystem", ROS_PACKAGE_NAME);
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
-      rviz_path + "/ogre_media/materials/scripts",
-      "FileSystem",
+      rviz_path + "/ogre_media/materials/scripts", "FileSystem",
       ROS_PACKAGE_NAME);
 
   // Add paths exported to the "media_export" package.
@@ -64,33 +59,30 @@ void ProbabilisticMeshVisual::initResourcePaths() {
       std::string path;
       int pos1 = 0;
       int pos2 = iter->find(delim);
-      while (pos2 != (int) std::string::npos) {
+      while (pos2 != (int)std::string::npos) {
         path = iter->substr(pos1, pos2 - pos1);
         ROS_DEBUG("adding resource location: '%s'\n", path.c_str());
-        Ogre::ResourceGroupManager::getSingleton().addResourceLocation(path,
-                                                                       "FileSystem",
-                                                                       ROS_PACKAGE_NAME);
+        Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+            path, "FileSystem", ROS_PACKAGE_NAME);
         pos1 = pos2 + 1;
         pos2 = iter->find(delim, pos2 + 1);
       }
       path = iter->substr(pos1, iter->size() - pos1);
       ROS_DEBUG("adding resource location: '%s'\n", path.c_str());
-      Ogre::ResourceGroupManager::getSingleton().addResourceLocation(path,
-                                                                     "FileSystem",
-                                                                     ROS_PACKAGE_NAME);
+      Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+          path, "FileSystem", ROS_PACKAGE_NAME);
     }
   }
 }
 
-void ProbabilisticMeshVisual::setMessage(const cgal_msgs::ProbabilisticMesh::ConstPtr& msg) {
+void ProbabilisticMeshVisual::setMessage(
+    const cgal_msgs::ProbabilisticMesh::ConstPtr& msg) {
   msg_ = msg;
 }
 
-ProbabilisticMeshVisual::~ProbabilisticMeshVisual() {
-}
+ProbabilisticMeshVisual::~ProbabilisticMeshVisual() {}
 
 void ProbabilisticMeshVisual::update() {
-
   Ogre::ManualObject* ogre_object;
 
   if (scene_manager_->hasManualObject(object_name_)) {
@@ -106,50 +98,39 @@ void ProbabilisticMeshVisual::update() {
   ogre_object->begin("BaseWhiteNoLighting",
                      Ogre::RenderOperation::OT_TRIANGLE_LIST);
 
+  // Displaying Vertices
   for (auto vrtx : msg_->vertices) {
-    ogre_object->position(vrtx.x,
-                          vrtx.y,
-                          vrtx.z);
+    ogre_object->position(vrtx.x, vrtx.y, vrtx.z);
 
     ogre_object->colour(surface_color_);
-
   }
+
+  // Displaying Triangles
   for (auto triangle : msg_->triangles) {
     ogre_object->triangle(triangle.vertex_indices[2],
                           triangle.vertex_indices[1],
                           triangle.vertex_indices[0]);
-
   }
   ogre_object->end();
 
+  // Displaying Edges
   ogre_object->begin("BaseWhiteNoLighting",
                      Ogre::RenderOperation::OT_LINE_LIST);
-
   for (auto triangle : msg_->triangles) {
-
     for (int i = 0; i < 3; ++i) {
-
       const auto& vrtx_a = msg_->vertices[triangle.vertex_indices[i]];
-      ogre_object->position(vrtx_a.x,
-                            vrtx_a.y,
-                            vrtx_a.z);
+      ogre_object->position(vrtx_a.x, vrtx_a.y, vrtx_a.z);
 
       ogre_object->colour(edge_color_);
 
       const auto& vrtx_b = msg_->vertices[triangle.vertex_indices[(i + 1) % 3]];
-      ogre_object->position(vrtx_b.x,
-                            vrtx_b.y,
-                            vrtx_b.z);
+      ogre_object->position(vrtx_b.x, vrtx_b.y, vrtx_b.z);
     }
-
   }
-
 
   // code to visualize covariances as lines. Disabled per default.
   if (visualize_covariances_) {
-
     for (size_t i = 0; i < msg_->vertices.size(); ++i) {
-
       Ogre::Vector3 mean;
       mean.x = msg_->vertices[i].x;
       mean.y = msg_->vertices[i].y;
@@ -162,18 +143,17 @@ void ProbabilisticMeshVisual::update() {
 
       Ogre::Vector3 start = mean - stdev, end = mean + stdev;
 
-      //x cov
+      // x cov
       ogre_object->position(start.x, mean.y, mean.z);
       ogre_object->position(end.x, mean.y, mean.z);
 
-      //y cov
+      // y cov
       ogre_object->position(mean.x, start.y, mean.z);
       ogre_object->position(mean.x, end.y, mean.z);
 
-      //z cov
+      // z cov
       ogre_object->position(mean.x, mean.y, start.z);
       ogre_object->position(mean.x, mean.y, end.z);
-
     }
   }
   Ogre::ManualObject::ManualObjectSection* section = ogre_object->end();
@@ -184,10 +164,8 @@ void ProbabilisticMeshVisual::update() {
   mat->setDepthWriteEnabled(false);
 
   // if backface culling is not desired, set culling mode to CULL_NONE
-  mat->setCullingMode(backface_culling_ ?
-                      Ogre::CullingMode::CULL_CLOCKWISE :
-                      Ogre::CullingMode::CULL_NONE);
-
+  mat->setCullingMode(backface_culling_ ? Ogre::CullingMode::CULL_CLOCKWISE
+                                        : Ogre::CullingMode::CULL_NONE);
 }
 
 void ProbabilisticMeshVisual::clear() {
