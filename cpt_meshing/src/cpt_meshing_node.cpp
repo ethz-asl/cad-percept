3d6816f859ba10d30e62ce11e2e674f12ca3559a
michaelpantic <michael.pantic@gmail.com>
2019-04-30 11:59:20 +0200

    Initial commit with an unfinished delaunay 3d mesher and boilerplate code

---
+++ cpt_meshing/src/cpt_meshing_node.cpp
@@ -0,0 +1,11 @@
+#include <cpt_meshing/mesher/delaunay_3d_mesher.h>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cad_percept::meshing::Delaunay3DMesher mesher;
+  cout << "Hello, World!";
+  return 0;
+}
