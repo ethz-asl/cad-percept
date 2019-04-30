e333c9e491f62f63f5f5064dfb1e467a5aba6fa5
Hermann <blumh@ethz.ch>
2019-04-30 10:34:30 +0200

    wip mesh to msg

---
+++ cgal_conversions/test/test_mesh_messages.cpp
@@ -0,0 +1,26 @@
+#include <cgal_conversions/mesh_conversions.h>
+#include <cgal_definitions/cgal_typedefs.h>
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+#include <cgal>
+
+#include <cgal_msgs/TriangleMesh.h>
+
+using namespace cad_percept::cgal;
+
+TEST(CGALConversionsTest, traingle_mesh_to_msg) {
+  SurfaceMesh m;
+  // create 4 vertices
+  vertex_descriptor a = m.add_vertex(Point(1, 0, 0));
+  vertex_descriptor b = m.add_vertex(Point(0, 1, 0));
+  vertex_descriptor c = m.add_vertex(Point(0, 0, 0));
+  vertex_descriptor d = m.add_vertex(Point(1, 1, 0));
+  // create 3 triangles
+  m.add_face(a, b, c);
+  m.add_face(b, c, d);
+  m.add_face(a, b, d);
+  // get message from mesh
+  TriangleMeshMsg msg = triangleMeshToMsg(m);
+  ExpectElementsAreArray(msg.vertices,
+                         {{1, 0, 0}, {0, 1, 0}, {0, 0, 0}, {1, 1, 0}});
+}
