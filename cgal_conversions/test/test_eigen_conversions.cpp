523ce54c9441f54f59c02943d3e9c78371c401b0
johnny <abel.gawel@mavt.ethz.ch>
2019-05-08 16:56:18 +0200

    Fixed tests.

--- cgal_conversions/test/test_eigen_conversions.cpp
+++ cgal_conversions/test/test_eigen_conversions.cpp
@@ -9,9 +9,34 @@ using namespace cad_percept::cgal;
 
 TEST(CGALConversionsTest, vector_to_eigen_vector) {
   //convert in both directions and compare
-  Eigen::Vector3d v_eigen(4,8,3);
-  Vector v_cgal = eigenVectorToVector(v_eigen);
-  Eigen::Vector3d v_eigen_comp = vectorToEigenVector(v_cgal);
+  Eigen::Vector3d v_eigen_a(Eigen::Vector3d::Random());
+  Vector v_cgal_a = eigenVectorToVector(v_eigen_a);
+  Eigen::Vector3d v_eigen_comp_a = vectorToEigenVector(v_cgal_a);
 
-  EXPECT_TRUE(v_eigen == v_eigen_comp);
+  EXPECT_TRUE(v_eigen_a == v_eigen_comp_a);
+
+  Eigen::Vector3d v_eigen_b(Eigen::Vector3d::Random());
+  Vector v_cgal_b;
+  eigenVectorToVector(&v_eigen_b, &v_cgal_b);
+  Eigen::Vector3d v_eigen_comp_b;
+  vectorToEigenVector(&v_cgal_b, &v_eigen_comp_b);
+
+  EXPECT_TRUE(v_eigen_b == v_eigen_comp_b);
+}
+
+TEST(CGALConversionsTest, point_to_eigen_vector) {
+  //convert in both directions and compare
+  Eigen::Vector3d v_eigen_a(Eigen::Vector3d::Random());
+  Point p_cgal_a = eigenVectorToCgalPoint(v_eigen_a);
+  Eigen::Vector3d v_eigen_comp_a = cgalPointToEigenVector(p_cgal_a);
+
+  EXPECT_TRUE(v_eigen_a == v_eigen_comp_a);
+
+  Eigen::Vector3d v_eigen_b(Eigen::Vector3d::Random());
+  Point p_cgal_b;
+  eigenVectorToCgalPoint(&v_eigen_b, &p_cgal_b);
+  Eigen::Vector3d v_eigen_comp_b;
+  cgalPointToEigenVector(&p_cgal_b, &v_eigen_comp_b);
+
+  EXPECT_TRUE(v_eigen_b == v_eigen_comp_b);
 }
