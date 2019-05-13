2f4632401d490be478de2df644700caa1fda76be
Julian Stiefel <44534970+jstiefel@users.noreply.github.com>
2019-05-13 18:46:46 +0200

    Added transformation conversions between cgal and eigen (#19)
    * added transformation conversions between cgal and eigen
    
    * removed redundant code
    
    * added conversions between TriangleMesh and ProbabilisticMesh msgs
    
    * adapted all cgal_conversions to style guide
    
    * remove unnecessary message conversions

--- cgal_conversions/include/cgal_conversions/eigen_conversions.h
+++ cgal_conversions/include/cgal_conversions/eigen_conversions.h
@@ -7,16 +7,22 @@ namespace cad_percept {
 namespace cgal {
 
 // Point <-> Vector
-void cgalPointToEigenVector(const Point *point, Eigen::Vector3d *vector);
-void eigenVectorToCgalPoint(const Eigen::Vector3d *vector, Point *point);
+void cgalPointToEigenVector(const Point &point, Eigen::Vector3d *vector);
+void eigenVectorToCgalPoint(const Eigen::Vector3d &vector, Point *point);
 Eigen::Vector3d cgalPointToEigenVector(const Point &point);
 Point eigenVectorToCgalPoint(const Eigen::Vector3d &vector);
 
 // Vector <-> Vector
-void cgalVectorToEigenVector(const Vector *cvector, Eigen::Vector3d *vector);
-void eigenVectorToCgalVector(const Eigen::Vector3d *vector, Vector *cvector);
+void cgalVectorToEigenVector(const Vector &cvector, Eigen::Vector3d *vector);
+void eigenVectorToCgalVector(const Eigen::Vector3d &vector, Vector *cvector);
 Eigen::Vector3d cgalVectorToEigenVector(const Vector &cvector);
 Vector eigenVectorToCgalVector(const Eigen::Vector3d &vector);
+
+// Transformation <-> Transformation
+void cgalTransformationToEigenTransformation(const Transformation &ctransformation, Eigen::Matrix4d *transformation);
+void eigenTransformationToCgalTransformation(const Eigen::Matrix4d &transformation, Transformation *ctransformation);
+Eigen::Matrix4d cgalTransformationToEigenTransformation(const Transformation &ctransformation);
+Transformation eigenTransformationToCgalTransformation(const Eigen::Matrix4d &transformation);
 
 }
 }
