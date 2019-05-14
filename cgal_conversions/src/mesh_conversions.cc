e104b1cea634c8d89f4cf13d13de9c9676af1260
Julian Stiefel <julian@pixelpress.ch>
2019-05-14 14:21:33 +0200

    adapted to master

--- cgal_conversions/src/mesh_conversions.cc
+++ cgal_conversions/src/mesh_conversions.cc
@@ -62,30 +62,6 @@ void triangleMeshToMsg(Polyhedron &m, cgal_msgs::TriangleMesh *msg) {
   }
 }
 
-// does not include any probabilities yet
-void triToProbMsg(const cgal_msgs::TriangleMesh &t_msg, cgal_msgs::ProbabilisticMesh *p_msg){
-  p_msg->mesh.triangles = t_msg.triangles;
-  p_msg->mesh.vertices = t_msg.vertices;
-}
-
-void probToTriMsg(const cgal_msgs::ProbabilisticMesh &p_msg, cgal_msgs::TriangleMesh *t_msg){
-  t_msg->triangles = p_msg.mesh.triangles;
-  t_msg->vertices = p_msg.mesh.vertices;
-}
-
-// does not include any probabilities yet
-void triangleMeshToProbMsg(Polyhedron &m, cgal_msgs::ProbabilisticMesh *p_msg){
-  cgal_msgs::TriangleMesh t_msg;
-  triangleMeshToMsg(m, &t_msg);
-  triToProbMsg(t_msg, p_msg);
-}
-
-void probMsgToTriangleMesh(const cgal_msgs::ProbabilisticMesh &p_msg, Polyhedron *m){
-  cgal_msgs::TriangleMesh t_msg;
-  probToTriMsg(p_msg, &t_msg);
-  msgToTriangleMesh(t_msg, m);
-}
-
 // A modifier creating a triangle with the incremental builder.
 template <class HDS>
 void BuildMesh<HDS>::operator()(HDS &hds) {
