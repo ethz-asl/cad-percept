f612adce28fbb17f1a6a1e1843474c68e647854c
Hermann <blumh@ethz.ch>
2019-05-14 15:43:55 +0200

    first version of modular visualization

--- cgal_visualizations/script/publish_mesh.sh
+++ cgal_visualizations/script/publish_mesh.sh
@@ -1,17 +1,11 @@
-rostopic pub /test_mesh cgal_msgs/ProbabilisticMesh "header:
+rostopic pub /test_mesh cgal_msgs/TriangleMeshStamped "header:
   seq: 0
   stamp: {secs: 0, nsecs: 0}
   frame_id: 'map'
-mesh:  
+mesh:
   triangles:
   - vertex_indices: [0, 1, 2]
   vertices:
   - {x: 0.0, y: 0.0, z: 0.0}
   - {x: 0.0, y: 1.0, z: 1.0}
-  - {x: 1.0, y: 1.0, z: 0.0}
-normals:
-- {x: 0.0, y: 0.0, z: 0.0}
-cov_vertices:
-- {x: 0.0, y: 0.0, z: 0.0}
-- {x: 0.0, y: 0.0, z: 0.0}
-- {x: 0.0, y: 0.0, z: 0.0}"
+  - {x: 1.0, y: 1.0, z: 0.0}"
