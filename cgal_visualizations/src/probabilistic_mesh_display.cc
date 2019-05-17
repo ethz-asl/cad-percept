8fece99f85015ebf04e6a81b859cc1f605d8b072
Hermann <blumh@ethz.ch>
2019-05-17 18:46:27 +0200

    test formatting hook

--- cgal_visualizations/src/probabilistic_mesh_display.cc
+++ cgal_visualizations/src/probabilistic_mesh_display.cc
@@ -12,7 +12,7 @@ namespace visualizations {
 
 void ProbabilisticMeshDisplay::onInitialize() {
   MFDClass::onInitialize();
-    initProperties();
+  initProperties();
 }
 
 ProbabilisticMeshDisplay::~ProbabilisticMeshDisplay() {}
@@ -27,38 +27,24 @@ void ProbabilisticMeshDisplay::reset() {
 }
 
 void ProbabilisticMeshDisplay::initProperties() {
-  properties_.BackfaceCulling = new rviz::BoolProperty("Backface Culling",
-                                                       false,
-                                                       "If backface culling is active, a surface is only visible from the front face defined by the normal",
-                                                       this,
-                                                       SLOT(
-                                                           backfaceCullingPropertyChanged())
-  );
+  properties_.BackfaceCulling = new rviz::BoolProperty(
+      "Backface Culling", false,
+      "If backface culling is active, a surface is only visible from the front "
+      "face defined by the normal",
+      this, SLOT(backfaceCullingPropertyChanged()));
 
-  properties_.SurfaceColor = new rviz::ColorProperty("Surface Color",
-                                                     QColor(0, 0, 255),
-                                                     "Color of surfaces.",
-                                                     this,
-                                                     SLOT(
-                                                         appearencePropertyChanged())
-  );
+  properties_.SurfaceColor = new rviz::ColorProperty(
+      "Surface Color", QColor(0, 0, 255), "Color of surfaces.", this,
+      SLOT(appearencePropertyChanged()));
 
-  properties_.EdgeColor = new rviz::ColorProperty("Edge Color",
-                                                  QColor(0, 0, 0),
-                                                  "Color of surfaces.",
-                                                  this,
-                                                  SLOT(
-                                                      appearencePropertyChanged())
-  );
+  properties_.EdgeColor = new rviz::ColorProperty(
+      "Edge Color", QColor(0, 0, 0), "Color of surfaces.", this,
+      SLOT(appearencePropertyChanged()));
 
-  properties_.Alpha = new rviz::FloatProperty("Alpha",
-                                              0.8,
-                                              "Transparency",
-                                              this,
-                                              SLOT(appearencePropertyChanged()));
+  properties_.Alpha = new rviz::FloatProperty(
+      "Alpha", 0.8, "Transparency", this, SLOT(appearencePropertyChanged()));
   properties_.Alpha->setMax(1.0);
   properties_.Alpha->setMin(0.0);
-
 }
 
 void ProbabilisticMeshDisplay::backfaceCullingPropertyChanged() {
@@ -70,11 +56,9 @@ void ProbabilisticMeshDisplay::backfaceCullingPropertyChanged() {
 
 void ProbabilisticMeshDisplay::appearencePropertyChanged() {
   if (visual_ != nullptr) {
-    visual_->setAppearance(
-        properties_.EdgeColor->getOgreColor(),
-        properties_.SurfaceColor->getOgreColor(),
-        properties_.Alpha->getFloat()
-    );
+    visual_->setAppearance(properties_.EdgeColor->getOgreColor(),
+                           properties_.SurfaceColor->getOgreColor(),
+                           properties_.Alpha->getFloat());
     visual_->update();
   }
 }
@@ -87,7 +71,7 @@ void ProbabilisticMeshDisplay::processMessage(
   Ogre::Quaternion orientation;
   Ogre::Vector3 position;
   if (!context_->getFrameManager()->getTransform(
-      msg->header.frame_id, msg->header.stamp, position, orientation)) {
+          msg->header.frame_id, msg->header.stamp, position, orientation)) {
     ROS_DEBUG("Error transforming from frame '%s' to frame '%s'",
               msg->header.frame_id.c_str(), qPrintable(fixed_frame_));
     return;
