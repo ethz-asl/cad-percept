eed4c1d832eb4657a25c2f43d51c571235698a29
michaelpantic <michael.pantic@gmail.com>
2019-06-10 23:16:46 +0200

    More cleanup, added Delaunay 2D triangulation

--- cpt_meshing/include/cpt_meshing/mesher/delaunay_3d_mesher.h
+++ cpt_meshing/include/cpt_meshing/mesher/delaunay_3d_mesher.h
@@ -5,51 +5,34 @@
 #include <CGAL/Delaunay_triangulation_cell_base_3.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Triangulation_vertex_base_with_info_3.h>
-
-#include <cgal_definitions/cgal_typedefs.h>
-#include <cpt_meshing/abstract_simple_mesher.h>
+#include <cpt_meshing/mesher/delaunay_xd_mesher.h>
 
 namespace cad_percept {
 namespace meshing {
+namespace Delaunay3DMesher {
 
-class Delaunay3DMesher : public AbstractSimpleMesher {
+typedef CGAL::Exact_predicates_inexact_constructions_kernel
+    ReconstructionKernel;
 
-  //Local CGAL Typedefs
-  typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned int,
-                                                      cad_percept::cgal::Kernel>
-      TriangulationVertexBase;
+//Local CGAL Typedefs
+typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned int,
+                                                    ReconstructionKernel>
+    TriangulationVertexBase;
 
-  typedef CGAL::Delaunay_triangulation_cell_base_3<cad_percept::cgal::Kernel>
-      TriangulationCellBase;
+typedef CGAL::Delaunay_triangulation_cell_base_3<ReconstructionKernel>
+    TriangulationCellBase;
 
-  typedef CGAL::Triangulation_data_structure_3<TriangulationVertexBase,
-                                               TriangulationCellBase>
-      TriangulationDataStructure;
+typedef CGAL::Triangulation_data_structure_3<TriangulationVertexBase,
+                                             TriangulationCellBase>
+    TriangulationDataStructure;
 
-  typedef CGAL::Delaunay_triangulation_3<cad_percept::cgal::Kernel,
-                                         TriangulationDataStructure>
-      DelaunayTriangulation;
+typedef CGAL::Delaunay_triangulation_3<ReconstructionKernel,
+                                       TriangulationDataStructure>
+    TriangulationType;
 
- public:
-  bool getMesh(cad_percept::cgal::Polyhedron* output,
-               MeshPerformanceCounters* counters = nullptr);
+typedef DelaunayXDMesher<TriangulationType> Mesher;
+}
 
- protected:
-
-  /* Internal helper class for conversion */
-  template<class HDS>
-  class Delaunay3DToPolyhedron : public CGAL::Modifier_base<HDS> {
-   public:
-    Delaunay3DToPolyhedron(const DelaunayTriangulation& triangulation) :
-        triangulation_(triangulation) {}
-
-    void operator()(HDS& hds);
-   private:
-    const DelaunayTriangulation& triangulation_;
-  };
-
-  std::vector<cgal::Point> vertices_;
-};
 }
 }
 #endif //CPT_MESHING_DELAUNAY_3D_MESHER_H
---
+++ cpt_meshing/include/cpt_meshing/mesher/delaunay_xd_mesher.h
@@ -0,0 +1,119 @@
+#ifndef CPT_MESHING_DELAUNAY_XD_MESHER_H
+#define CPT_MESHING_DELAUNAY_XD_MESHER_H
+
+#include <cgal_definitions/cgal_typedefs.h>
+#include <cpt_meshing/abstract_simple_mesher.h>
+
+namespace cad_percept {
+namespace meshing {
+
+template<class DelaunayTriangulation>
+class DelaunayXDMesher : public AbstractSimpleMesher {
+
+ public:
+  bool getMesh(cad_percept::cgal::Polyhedron* output,
+               MeshPerformanceCounters* counters = nullptr) {
+    if (!inputPointsValid()) {
+      return false;
+    }
+
+    using clock = std::chrono::steady_clock;
+    clock::time_point start = clock::now();
+
+    // set up triangulation datastructure.
+    typename DelaunayTriangulation::Vertex_handle vh;
+    DelaunayTriangulation dt;
+
+    // perform triangulation
+    for (size_t i = 0; i < points_->size(); ++i) {
+
+      // Create Point3 from pcl
+      // Todo: Add PCL<->CGAL conversions to cgal_conversions
+      typename DelaunayTriangulation::Point
+          vertex(points_->at(i).x, points_->at(i).y, points_->at(i).z);
+
+      vh = dt.insert(vertex);
+      vh->info() = i; // Assign index as info.
+    }
+
+
+    // convert to SurfaceMesh
+    output->erase_all();
+    DelaunayXDToPolyhedron<cad_percept::cgal::Polyhedron::HalfedgeDS>
+        converter(dt);
+    output->delegate(converter);
+    CGAL::set_halfedgeds_items_id(*output); // fix ids
+
+    clock::time_point end = clock::now();
+    std::chrono::duration<double, std::milli> execution_time = end - start;
+    if (counters != nullptr) {
+      counters->processing_time = execution_time.count();
+    }
+
+    return true;
+  }
+
+ protected:
+
+  /* Internal helper class for conversion */
+  template<class HDS>
+  class DelaunayXDToPolyhedron : public CGAL::Modifier_base<HDS> {
+   public:
+    DelaunayXDToPolyhedron(const DelaunayTriangulation& triangulation) :
+        triangulation_(triangulation) {}
+
+    void operator()(HDS& hds) {
+
+      size_t num_vertices = triangulation_.number_of_vertices();
+      size_t num_facets = triangulation_.number_of_faces();
+      std::cout << "NUM_VERTICES: " << num_vertices << std::endl;
+
+      //  Build a builder and begin construction
+      CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+      B.begin_surface(num_vertices, num_facets);
+
+      // add all vertices and check index
+      for (typename DelaunayTriangulation::Finite_vertices_iterator
+               vit = triangulation_.finite_vertices_begin();
+           vit != triangulation_.finite_vertices_end(); ++vit) {
+
+        typename DelaunayTriangulation::Point
+            r_point = triangulation_.point(vit);
+
+        double x = r_point.x();
+        double y = r_point.y();
+        double z = r_point.z();
+
+        typename HDS::Vertex_handle vh_new = B.add_vertex(cgal::Point(x, y, z));
+        vh_new->id() = vit->info();
+        std::cout << vit->info() << std::endl;
+      }
+
+      uint cells, facets;
+
+      // Iterate through all triangles and add them
+      for (typename DelaunayTriangulation::Face_iterator
+               fit = triangulation_.faces_begin();
+           fit != triangulation_.faces_end(); ++fit) {
+
+
+        // Create new facet and add vertices
+        B.begin_facet();
+
+        B.add_vertex_to_facet(fit->vertex(0)->info());
+        B.add_vertex_to_facet(fit->vertex(1)->info());
+        B.add_vertex_to_facet(fit->vertex(2)->info());
+        B.end_facet();
+      }
+
+      B.end_surface();
+    }
+
+   private:
+    const DelaunayTriangulation& triangulation_;
+  };
+
+};
+}
+}
+#endif //CPT_MESHING_DELAUNAY_XD_MESHER_H
