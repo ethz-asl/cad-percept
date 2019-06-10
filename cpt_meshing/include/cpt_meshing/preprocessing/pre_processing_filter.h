330ebd27595a2c722289a31c6e7e05d6550a0cdf
michaelpantic <michael.pantic@gmail.com>
2019-06-10 13:50:55 +0200

    Cleanup & refactoring

--- cpt_meshing/include/cpt_meshing/preprocessing/pre_processing_filter.h
+++ cpt_meshing/include/cpt_meshing/preprocessing/pre_processing_filter.h
@@ -7,19 +7,32 @@ namespace meshing {
 
 class PreProcessingFilter {
  public:
+  PreProcessingFilter();
+
   void run(const InputPointCloud::ConstPtr input,
            InputPointCloud::Ptr output,
            InputNormals::Ptr normals);
 
+  // Adds a simple box filter to constrain axes
   void addBoxFilter(const std::string& axis,
                     double min,
                     double max);
 
+  // adds a voxel integration filter to the pipeline
   void addVoxelFilter(double x_leaf, double y_leaf, double z_leaf);
 
+  void setNormalEstimationRadius(const double radius){
+    normal_search_radius_ = radius;
+  }
+
+
  private:
-  void estimateNormals(InputPointCloud::ConstPtr input, InputNormals::Ptr normals);
+  // Convenience method for normal estimation.
+  void estimateNormals(InputPointCloud::ConstPtr input,
+                       InputNormals::Ptr normals);
+
   std::vector<std::shared_ptr<InputPointCloudFilter>> filter_list_;
+  double normal_search_radius_; // radius for estimating normals in [m]
 };
 }
 }
