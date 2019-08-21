126b5b3ad964cd7dce56f51db634cd6549fe4f24
Hermann <blumh@ethz.ch>
2019-08-21 17:04:22 +0200

    print meshfile path

--- cpt_meshcad_interface/src/cad_interface.cc
+++ cpt_meshcad_interface/src/cad_interface.cc
@@ -10,6 +10,7 @@ CadInterface::CadInterface(ros::NodeHandle& n) : nh_(n), iterator_(0) {
 CadInterface::~CadInterface() {};
 
 void CadInterface::load(const std::string &filename) {
+  std::cout << "INFO: Loading CAD file from " << filename << std::endl;
   // Loading of mesh data.
   std::ifstream off_file(filename.c_str(), std::ios::binary);
   if (!CGAL::read_off(off_file, P_)) {
@@ -35,7 +36,7 @@ void CadInterface::publishPointCloudThread() {
     ros::Rate thread_rate(0.5);
     while (ros::ok()) {
 
-      // Publish 
+      // Publish
       cad_mesh_msg_.header.stamp = ros::Time::now();
       cad_mesh_msg_.header.seq = iterator_++;
       cad_mesh_pub_.publish(cad_mesh_msg_);
