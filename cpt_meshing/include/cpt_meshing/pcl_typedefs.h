adb9fc6c8ef68dd52114044e31be367a8a098653
michaelpantic <michael.pantic@gmail.com>
2019-08-27 18:41:28 +0200

    Applied style patch

--- cpt_meshing/include/cpt_meshing/pcl_typedefs.h
+++ cpt_meshing/include/cpt_meshing/pcl_typedefs.h
@@ -1,9 +1,9 @@
 #ifndef CPT_MESHING_PCL_TYPEDEFS_H
 #define CPT_MESHING_PCL_TYPEDEFS_H
 
-#include <pcl/point_types.h>
-#include <pcl/filters/filter.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/filters/filter.h>
+#include <pcl/point_types.h>
 
 namespace cad_percept {
 namespace meshing {
@@ -13,7 +13,6 @@ typedef pcl::PointCloud<InputPoint> InputPointCloud;
 typedef pcl::PointCloud<pcl::Normal> InputNormals;
 
 typedef pcl::Filter<InputPoint> InputPointCloudFilter;
-
 }
 }
-#endif //CPT_MESHING_PCL_TYPEDEFS_H
+#endif  // CPT_MESHING_PCL_TYPEDEFS_H
--- cpt_meshing/src/meshing_node.cc
+++ cpt_meshing/src/meshing_node.cc
@@ -4,16 +4,13 @@ namespace cad_percept {
 namespace meshing {
 
 CadPerceptMeshingNode::CadPerceptMeshingNode(ros::NodeHandle nh,
-                                             ros::NodeHandle nh_private) :
-    nh_(nh),
-    nh_private_(nh_private) {
-
+                                             ros::NodeHandle nh_private)
+    : nh_(nh), nh_private_(nh_private) {
   // only one meshing type at the moment.
   mesher_ = std::make_shared<Delaunay2DMesher::Mesher>();
 
   // set up box filter if needed.
   if (nh_private_.param<bool>("box_filter/enable", false)) {
-
     double min, max;
     if (nh_private_.getParam("box_filter/x_min", min) &&
         nh_private_.getParam("box_filter/x_max", max)) {
@@ -39,26 +36,21 @@ CadPerceptMeshingNode::CadPerceptMeshingNode(ros::NodeHandle nh,
     double voxel_x = nh_private_.param<double>("voxel_filter/x_size", 0.02);
     double voxel_y = nh_private_.param<double>("voxel_filter/y_size", 0.02);
     double voxel_z = nh_private_.param<double>("voxel_filter/z_size", 0.02);
-    ROS_INFO("Adding Voxel filter with size [%.2f, %.2f, %.2f].",
-             voxel_x,
-             voxel_y,
-             voxel_z);
+    ROS_INFO("Adding Voxel filter with size [%.2f, %.2f, %.2f].", voxel_x,
+             voxel_y, voxel_z);
     preprocessing_.addVoxelFilter(voxel_x, voxel_y, voxel_z);
   }
 
-  preprocessing_.setNormalEstimationRadius(nh_private_.param<double>(
-      "normal_estimation/radius",
-      0.03));
-  sub_points_ = nh_.subscribe("points",
-                              1,
-                              &CadPerceptMeshingNode::pointCoudCallback,
-                              this);
+  preprocessing_.setNormalEstimationRadius(
+      nh_private_.param<double>("normal_estimation/radius", 0.03));
+  sub_points_ = nh_.subscribe("points", 1,
+                              &CadPerceptMeshingNode::pointCoudCallback, this);
 
   pub_mesh_ = nh.advertise<cgal_msgs::TriangleMeshStamped>("mesh", 1);
 }
 
-void CadPerceptMeshingNode::pointCoudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
-
+void CadPerceptMeshingNode::pointCoudCallback(
+    const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
   cad_percept::meshing::InputPointCloud::Ptr input_cloud =
       boost::make_shared<cad_percept::meshing::InputPointCloud>();
 
@@ -70,8 +62,8 @@ void CadPerceptMeshingNode::pointCoudCallback(const sensor_msgs::PointCloud2Cons
   // create datastructures
   cad_percept::meshing::InputPointCloud::Ptr cloud_filtered =
       boost::make_shared<cad_percept::meshing::InputPointCloud>();
-  cad_percept::meshing::InputNormals::Ptr
-      normals = boost::make_shared<cad_percept::meshing::InputNormals>();
+  cad_percept::meshing::InputNormals::Ptr normals =
+      boost::make_shared<cad_percept::meshing::InputNormals>();
 
   cad_percept::cgal::Polyhedron mesh;
 
@@ -89,6 +81,5 @@ void CadPerceptMeshingNode::pointCoudCallback(const sensor_msgs::PointCloud2Cons
   cgal::triangleMeshToMsg(mesh, &msg.mesh);
   pub_mesh_.publish(msg);
 }
-
 }
 }
