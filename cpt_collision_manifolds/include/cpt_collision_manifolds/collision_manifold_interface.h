341ff998d2028101a768804c845550f5ad6615b0
michaelpantic <michael.pantic@gmail.com>
2019-08-28 08:39:04 +0200

    Construction method in interface added

--- cpt_collision_manifolds/include/cpt_collision_manifolds/collision_manifold_interface.h
+++ cpt_collision_manifolds/include/cpt_collision_manifolds/collision_manifold_interface.h
@@ -20,12 +20,20 @@ class CollisionManifoldInterface {
   virtual double signedDistance(const Eigen::Vector3d& position) = 0;
 
   /*
-   * Constructs the collision manifold as a mesh,
+   * Outputs the collision manifold as a mesh,
    * based on the current body attitude.
    */
   virtual void getAsMesh(cgal::PolyhedronPtr mesh) = 0;
 
+  /*
+   * Method that triggers the construction of the underlying
+   * Manifold. This is the method where most of the computation
+   * should happen.
+   */
+  virtual void construct() = 0;
+
+
 };
 }
 }
-#endif //CPT_COLLISION_MANIFOLDS_COLLISION_MANIFOLD_INTERFACE_H
+#endif // CPT_COLLISION_MANIFOLDS_COLLISION_MANIFOLD_INTERFACE_H
