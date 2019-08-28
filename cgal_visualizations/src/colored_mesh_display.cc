1fa0debec29cdd980b6e70c54e902ecb4c136230
Hermann <blumh@ethz.ch>
2019-08-28 14:03:45 +0200

    set max line length to 100

--- cgal_visualizations/src/colored_mesh_display.cc
+++ cgal_visualizations/src/colored_mesh_display.cc
@@ -34,11 +34,10 @@ void ColoredMeshDisplay::initProperties() {
       this, SLOT(backfaceCullingPropertyChanged()));
 
   properties_.EdgeColor = new rviz::ColorProperty(
-      "Edge Color", QColor(0, 0, 0), "Color of surfaces.", this,
-      SLOT(appearencePropertyChanged()));
+      "Edge Color", QColor(0, 0, 0), "Color of surfaces.", this, SLOT(appearencePropertyChanged()));
 
-  properties_.Alpha = new rviz::FloatProperty(
-      "Alpha", 0.8, "Transparency", this, SLOT(appearencePropertyChanged()));
+  properties_.Alpha = new rviz::FloatProperty("Alpha", 0.8, "Transparency", this,
+                                              SLOT(appearencePropertyChanged()));
   properties_.Alpha->setMax(1.0);
   properties_.Alpha->setMin(0.0);
 }
@@ -53,23 +52,21 @@ void ColoredMeshDisplay::backfaceCullingPropertyChanged() {
 void ColoredMeshDisplay::appearencePropertyChanged() {
   if (visual_ != nullptr) {
     visual_->setAppearance(properties_.EdgeColor->getOgreColor(),
-                           Ogre::ColourValue(0.0, 0.0, 1.0, 0.8),
-                           properties_.Alpha->getFloat());
+                           Ogre::ColourValue(0.0, 0.0, 1.0, 0.8), properties_.Alpha->getFloat());
     visual_->update();
   }
 }
 
-void ColoredMeshDisplay::processMessage(
-    const cgal_msgs::ColoredMesh::ConstPtr &msg) {
+void ColoredMeshDisplay::processMessage(const cgal_msgs::ColoredMesh::ConstPtr &msg) {
   // Here we call the rviz::FrameManager to get the transform from the
   // fixed frame to the frame in the header of this Imu message.  If
   // it fails, we can't do anything else so we return.
   Ogre::Quaternion orientation;
   Ogre::Vector3 position;
-  if (!context_->getFrameManager()->getTransform(
-          msg->header.frame_id, msg->header.stamp, position, orientation)) {
-    ROS_DEBUG("Error transforming from frame '%s' to frame '%s'",
-              msg->header.frame_id.c_str(), qPrintable(fixed_frame_));
+  if (!context_->getFrameManager()->getTransform(msg->header.frame_id, msg->header.stamp, position,
+                                                 orientation)) {
+    ROS_DEBUG("Error transforming from frame '%s' to frame '%s'", msg->header.frame_id.c_str(),
+              qPrintable(fixed_frame_));
     return;
   }
 
@@ -88,5 +85,4 @@ void ColoredMeshDisplay::processMessage(
 }  // namespace cad_percept
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(cad_percept::visualizations::ColoredMeshDisplay,
-                       rviz::Display)
+PLUGINLIB_EXPORT_CLASS(cad_percept::visualizations::ColoredMeshDisplay, rviz::Display)
