b6bbebb67d0946e3e0f8ce9c966fc5ae1d164163
michaelpantic <michael.pantic@gmail.com>
2019-08-29 20:05:28 +0200

    Change interface from TriangleMesh to TriangleMeshStamped

--- cpt_collision_manifolds/src/collision_manifold_test_node.cc
+++ cpt_collision_manifolds/src/collision_manifold_test_node.cc
@@ -1,5 +1,6 @@
 #include <cgal_conversions/mesh_conversions.h>
 #include <cgal_msgs/TriangleMesh.h>
+#include <cgal_msgs/TriangleMeshStamped.h>
 #include <cpt_collision_manifolds/iso_surface_manifold.h>
 #include <ros/ros.h>
 
@@ -19,13 +20,13 @@ class CollisionManifoldTestNode {
 
     // Publisher for the constructed manifold.
     pub_collision_manifold_ =
-        nh_private_.advertise<cgal_msgs::TriangleMesh>("collision_manifold", 1);
+        nh_private_.advertise<cgal_msgs::TriangleMeshStamped>("collision_manifold", 1);
   }
 
  private:
-  void meshCallback(const cgal_msgs::TriangleMeshConstPtr& mesh) {
+  void meshCallback(const cgal_msgs::TriangleMeshStampedConstPtr& mesh) {
     cgal::PolyhedronPtr original_surface = std::make_shared<cgal::Polyhedron>();
-    cgal::msgToTriangleMesh(*mesh, original_surface.get());
+    cgal::msgToTriangleMesh(mesh->mesh, original_surface.get());
 
     // Create construction strategy
     auto construction_strategy = std::make_shared<offset_surface::VertexNormalStrategy>();
@@ -40,8 +41,9 @@ class CollisionManifoldTestNode {
     collision_manifold.getAsMesh(collision_manifold_mesh.get());
 
     // send message with collision manifold
-    cgal_msgs::TriangleMesh output_msg;
-    cgal::triangleMeshToMsg(*collision_manifold_mesh, &output_msg);
+    cgal_msgs::TriangleMeshStamped output_msg;
+    output_msg.header = mesh->header;
+    cgal::triangleMeshToMsg(*collision_manifold_mesh, &output_msg.mesh);
     pub_collision_manifold_.publish(output_msg);
   }
 
