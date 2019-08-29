7b41d445b4ae586a192776267f4cf869c9248667
michaelpantic <michael.pantic@gmail.com>
2019-08-29 18:55:10 +0200

    Tested version with comments

--- cpt_utils/include/cpt_utils/mesh_publisher.h
+++ cpt_utils/include/cpt_utils/mesh_publisher.h
@@ -5,21 +5,30 @@
 #include <cgal_conversions/mesh_conversions.h>
 #include <cgal_definitions/cgal_typedefs.h>
 #include <cgal_definitions/mesh_model.h>
+#include <cgal_msgs/TriangleMeshStamped.h>
 #include <ros/ros.h>
 #include <std_srvs/Trigger.h>
 
 namespace cad_percept {
 namespace cpt_utils {
 
+/*
+ * Simple node that reads off files and publishes them as TriangleMeshStamped.
+ */
 class MeshPublisher {
  public:
-  MeshPublisher(ros::NodeHandle nh, ros::NodeHandle nh_private) : nh_(nh), nh_private_(nh_private) {
-    pub_mesh_ = nh_.advertise<cgal_msgs::TriangleMesh>("mesh_out", 1, true);
-    publish_service_ = nh_.advertiseService("publish", &MeshPublisher::triggerService, this);
-    default_filename_ = nh_private_.param<std::string>("default_filename", "mesh.off");
-  }
+  MeshPublisher(ros::NodeHandle nh, ros::NodeHandle nh_private);
 
+  /*
+   * Reads off file and publishes it via pub_mesh_
+   */
   bool publishOffFile(const std::string filename);
+
+  /*
+   * Service callback.
+   * Currently it always calls publishOffFile with the default_filename_.
+   * todo: Later we should add a service call with a path parameter.
+   */
   bool triggerService(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
 
  private:
@@ -28,6 +37,7 @@ class MeshPublisher {
   ros::ServiceServer publish_service_;
   ros::Publisher pub_mesh_;
   std::string default_filename_;
+  std::string frame_name_;
 };
 }  // namespace cpt_utils
 }  // namespace cad_percept
--- cpt_utils/src/mesh_publisher.cc
+++ cpt_utils/src/mesh_publisher.cc
@@ -4,19 +4,32 @@
 
 namespace cad_percept {
 namespace cpt_utils {
+
+MeshPublisher::MeshPublisher(ros::NodeHandle nh, ros::NodeHandle nh_private)
+    : nh_(nh), nh_private_(nh_private) {
+  pub_mesh_ = nh_private_.advertise<cgal_msgs::TriangleMeshStamped>("mesh_out", 1, true);
+  publish_service_ = nh_private_.advertiseService("publish", &MeshPublisher::triggerService, this);
+  default_filename_ = nh_private_.param<std::string>("default_filename", "mesh.off");
+  frame_name_ = nh_private_.param<std::string>("frame_name", "world");
+}
+
 bool MeshPublisher::publishOffFile(const std::string filename) {
   std::ifstream infile(filename.c_str());
+
+  // Check if file is readable.
   if (!infile.good()) {
     return false;
   }
 
   // Read Off file into polyhedron type
   cgal::Polyhedron mesh;
-  infile >> mesh;
+  infile >> mesh;  // such a nice operator overload!
 
   // publish as mesh msg
-  cgal_msgs::TriangleMesh mesh_msg;
-  cgal::triangleMeshToMsg(mesh, &mesh_msg);
+  cgal_msgs::TriangleMeshStamped mesh_msg;
+  mesh_msg.header.stamp = ros::Time::now();
+  mesh_msg.header.frame_id = frame_name_;
+  cgal::triangleMeshToMsg(mesh, &mesh_msg.mesh);
   pub_mesh_.publish(mesh_msg);
   return true;
 }
