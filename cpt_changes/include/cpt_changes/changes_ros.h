a6266fa2adc458c8f4a8f9672200bd70a56bba25
michaelpantic <michael.pantic@gmail.com>
2019-09-02 22:18:02 +0200

    Adjust code base for mesh-model interface change

--- cpt_changes/include/cpt_changes/changes_ros.h
+++ cpt_changes/include/cpt_changes/changes_ros.h
@@ -46,7 +46,7 @@ class ChangesRos {
 
  private:
   ros::NodeHandle &nh_, nh_private_;
-  cgal::MeshModel mesh_model_;
+  cgal::MeshModel::Ptr   mesh_model_;
   ros::Publisher good_matches_pub_, bad_matches_pub_, model_pub_, arch_pub_, mesh_pub_,
       distance_triangles_pub_;
   ros::Subscriber pointcloud_sub_;
