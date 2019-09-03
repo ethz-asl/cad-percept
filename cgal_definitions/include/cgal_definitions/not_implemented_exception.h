bad2191b73c3521e85c3c61033bac214be8a1a2a
michaelpantic <michael.pantic@gmail.com>
2019-09-03 16:38:17 +0200

    Addressed comments / added a NotImplementedException plus changed todo's

---
+++ cgal_definitions/include/cgal_definitions/not_implemented_exception.h
@@ -0,0 +1,28 @@
+#ifndef CGAL_DEFINITIONS_EXCEPTIONS_H_
+#define CGAL_DEFINITIONS_EXCEPTIONS_H_
+
+/*
+ * Simple NotImplementedException as seen on
+ * https://stackoverflow.com/questions/24469927
+ */
+namespace cad_percept {
+class NotImplementedException : public std::logic_error {
+ private:
+  std::string _text;
+
+  NotImplementedException(const char* message, const char* function)
+      : std::logic_error("Not Implemented") {
+    _text = message;
+    _text += " : ";
+    _text += function;
+  };
+
+ public:
+  NotImplementedException() : NotImplementedException("Not Implememented", __FUNCTION__) {}
+
+  NotImplementedException(const char* message) : NotImplementedException(message, __FUNCTION__) {}
+
+  virtual const char* what() const throw() { return _text.c_str(); }
+};
+}  // namespace cad_percept
+#endif  // CGAL_DEFINITIONS_EXCEPTIONS_H_
