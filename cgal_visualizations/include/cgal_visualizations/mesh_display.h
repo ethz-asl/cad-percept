dcbce0507bf859520a5f6958623e89524503b7d1
michaelpantic <michael.pantic@gmail.com>
2019-09-09 16:42:39 +0200

    Fix Q_OBJECT / moc-compiler issues

--- cgal_visualizations/include/cgal_visualizations/mesh_display.h
+++ cgal_visualizations/include/cgal_visualizations/mesh_display.h
@@ -2,13 +2,12 @@
 #define CGAL_VISUALIZATIONS_MESH_DISPLAY_H_
 
 #include <cgal_msgs/TriangleMeshStamped.h>
+#include <cgal_visualizations/mesh_visual.h>
 #include <rviz/message_filter_display.h>
 #include <rviz/properties/bool_property.h>
 #include <rviz/properties/color_property.h>
 #include <rviz/properties/float_property.h>
 #include <Eigen/Dense>
-#include "mesh_visual.h"
-#include "q_mesh_display.h"
 
 namespace cad_percept {
 namespace visualizations {
@@ -21,37 +20,29 @@ class MeshVisual;  // Forward definition
  *  a) T is a valid ROS Message Type with Header
  *  b) MeshVisual has an overload to handle whatever T is.
  *
- *  There's a bit of a peculiarity in the implementation of this.
- *  - QMeshDisplay handles all QT special things (implementation of Q_Object!)
- *    As Q_Objects cannot be templated, this is handled in a separate abstract base that is not
- *    templated.
- *
- *  - MeshDisplay then inherits from TWO classes, MessageFilterDisplay for all the RVIZ things, and
- *    QMeshDisplay for all QT specific stuff.
- *
- *  One of the few occurences where multiple inheritance really solves a problem ;-) So MeshDisplay
- *  has the behaviour of both independent base classes.
+ *   Note that MeshDisplay is not a valid QObject (yet) to be used in RVIZ.
+ *   This happens in the explicit template instantiations in
+ *   mesh_display_instantiations.h
  */
 template <class T>
-class MeshDisplay : public rviz::MessageFilterDisplay<T>, public QMeshDisplay {
+class MeshDisplay : public rviz::MessageFilterDisplay<T> {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   MeshDisplay();
-  virtual ~MeshDisplay();
+  ~MeshDisplay();
 
   void initProperties();
 
  protected:
   // Property Callbacks
-  void backfaceCullingPropertyChanged() final;
-  void appearencePropertyChanged() final;
+  virtual void backfaceCullingPropertyChanged();
+  virtual void appearencePropertyChanged();
 
-  virtual void onInitialize();
-  virtual void reset();
+  void onInitialize() override;
+  void reset() override;
 
- private:
   void updateVisualProperties();
-  void processMessage(const typename T::ConstPtr& msg);
+  void processMessage(const typename T::ConstPtr& msg) override;
 
   struct {
     rviz::BoolProperty* BackfaceCulling;
@@ -62,15 +53,6 @@ class MeshDisplay : public rviz::MessageFilterDisplay<T>, public QMeshDisplay {
   } properties_;
   std::unique_ptr<MeshVisual> visual_;
 };
-
-/*
- * That's where the magic happens -
- *   MeshDisplay typedefd with the different Message types in order to obtain
- *   the exportable, pluginlib compatible full implementations.
- */
-typedef MeshDisplay<cgal_msgs::TriangleMeshStamped> TriangleMeshDisplay;
-typedef MeshDisplay<cgal_msgs::ProbabilisticMesh> ProbabilisticMeshDisplay;
-typedef MeshDisplay<cgal_msgs::ColoredMesh> ColoredMeshDisplay;
 
 }  // namespace visualizations
 }  // namespace cad_percept
