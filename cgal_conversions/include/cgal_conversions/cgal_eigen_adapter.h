76816dbae710140d1f8ad4988e6ebf4ae4bd78ce
michaelpantic <michael.pantic@gmail.com>
2019-09-11 23:40:18 +0200

    Templated version with 2d/3d support

--- cgal_conversions/include/cgal_conversions/cgal_eigen_adapter.h
+++ cgal_conversions/include/cgal_conversions/cgal_eigen_adapter.h
@@ -10,6 +10,9 @@ namespace cgal {
  * This header-only library provides functionality to design functions such that they
  * automatically accept eigen and cgal vectors without templates/overloading and the like.
  * Makes Eigen::Vector3d / cgal::Vector_3 / cgal::Point_3 interchangeable.
+ * See Typedefs at end of file as well. Now it is templated for 2d and/or 3d vectors.
+ * All following examples are for 3d vectors, but usage is the same with 2d vectors
+ * (exchange Vector3In with Vector2In etc).
  *
  * Examples:
  * You want a function that accepts a Vector3 (regardless of eigen/cgal):
@@ -40,20 +43,84 @@ namespace cgal {
 /*
  * Abstract base class for functionality
  * Contains x,y,z data for a vector.
- * Can currently only deal with 3d, non-homogenous data. (hz = 0 in cgal)
+ * Can currently only deal with 2d and 3d, non-homogenous data. (hz = 0 in cgal)
  */
-class Vector3Adap {
+
+template <int N>
+class VectorAdap {
+  // Make sure this only builds for N=2 or N=3.
+  static_assert(N == 2 || N == 3, "ONLY IMPLEMENTED FOR 2D/3D VECTOR TYPES.");
+
  public:
-  Vector3Adap(double x, double y, double z) : x_(x), y_(y), z_(z) {}
+  /*
+   * Conditional typedefs to always get the right cgal/eigen type for our needs / template
+   * specializations.
+   */
+  typedef typename std::conditional<
+      /*    */ (N == 2),
+      /* y? */ cgal::Vector_2,
+      /* n? */ cgal::Vector>::type cgalVector;
+
+  typedef typename std::conditional<
+      /*    */ (N == 2),
+      /* y? */ cgal::Point_2,
+      /* n? */ cgal::Point>::type cgalPoint;
+
+  typedef typename std::conditional<
+      /*    */ (N == 2),
+      /* y? */ Eigen::Vector2d,
+      /* n? */ Eigen::Vector3d>::type eigenVector;
 
  protected:
-  inline Vector3Adap(cgal::Vector vec) : Vector3Adap(vec.x(), vec.y(), vec.z()) {}
-  inline Vector3Adap(cgal::Point pt) : Vector3Adap(pt.x(), pt.y(), pt.z()) {}
-  inline Vector3Adap(Eigen::Vector3d eigen) : Vector3Adap(eigen.x(), eigen.y(), eigen.z()) {}
+  inline VectorAdap() {}
+
+  inline VectorAdap(cgalVector vec) : VectorAdap() { toData(vec, data_); }
+  inline VectorAdap(cgalPoint pt) : VectorAdap() { toData(pt, data_); }
+  inline VectorAdap(eigenVector eigen) : VectorAdap() { toData(eigen, data_); }
+
+  /*
+   * Methods that decouple the conversion from/to the internal datastructure
+   * and allow template specialization as we need it here.
+   */
+  template <class T>
+  inline static void toData(const T& vec, double* data);
+
+  template <class T>
+  inline static void fromData(const double* data, T* vec);
 
   virtual void dummy() = 0;  // method to mark this class as purely abstract
-  double x_, y_, z_;
+  double data_[N];
 };
+
+/* Template specializations for toData
+ * Compiles for all T that have that same interface (which by chance is true with eigen/cgal here).
+ */
+template <> // template instantiation on class level, thus empty <>
+template <class T>  // normal template on function level
+inline void VectorAdap<3>::toData(const T& vec, double* data) {
+  data[0] = vec.x();
+  data[1] = vec.y();
+  data[2] = vec.z();
+}
+
+template <>
+template <class T>
+inline void VectorAdap<2>::toData(const T& vec, double* data) {
+  data[0] = vec.x();
+  data[1] = vec.y();
+}
+
+/* Template specializations for fromData */
+template <>
+template <class T>
+inline void VectorAdap<3>::fromData(const double* data, T* vec) {
+  *vec = {data[0], data[1], data[2]};
+}
+template <>
+template <class T>
+inline void VectorAdap<2>::fromData(const double* data, T* vec) {
+  *vec = {data[0], data[1]};
+}
 
 /*
  * Class that supports automatic casting from a
@@ -64,55 +131,52 @@ class Vector3Adap {
  *  and allows assignments from these types that are reflected back to the pointer.
  *
  */
-class Vector3Out : public Vector3Adap {
+template <int N>
+class VectorOut : public VectorAdap<N> {
  public:
   /*
    * Implicit cast-constructors for all 3 types.
    */
-  inline Vector3Out(cgal::Vector* vec)
-      : Vector3Adap(*vec),
+  inline VectorOut(typename VectorAdap<N>::cgalVector* vec)
+      : VectorAdap<N>(*vec),
         external_eigen_(nullptr),
         external_cgal_(vec),
         external_point_(nullptr) {}
 
-  inline Vector3Out(Eigen::Vector3d* vec)
-      : Vector3Adap(*vec),
+  inline VectorOut(typename VectorAdap<N>::eigenVector* vec)
+      : VectorAdap<N>(*vec),
         external_eigen_(vec),
         external_cgal_(nullptr),
         external_point_(nullptr) {}
 
-  inline Vector3Out(cgal::Point* pt)
-      : Vector3Adap(*pt), external_eigen_(nullptr), external_cgal_(nullptr), external_point_(pt) {}
+  inline VectorOut(typename VectorAdap<N>::cgalPoint* pt)
+      : VectorAdap<N>(*pt),
+        external_eigen_(nullptr),
+        external_cgal_(nullptr),
+        external_point_(pt) {}
 
   // Disable assignment of Vector3Out itself
-  void operator=(const Vector3Out&) = delete;
+  void operator=(const VectorOut&) = delete;
 
   /*
    * Assignment operators for all 3 types.
    */
-  inline void operator=(const Eigen::Vector3d& value) {
-    x_ = value.x();
-    y_ = value.y();
-    z_ = value.z();
+  inline void operator=(const typename VectorAdap<N>::eigenVector& value) {
+    this->toData(value, this->data_);
+    sync();
+  }
+  inline void operator=(const typename VectorAdap<N>::cgalVector& value) {
+    this->toData(value, this->data_);
     sync();
   }
 
-  inline void operator=(const cgal::Vector& value) {
-    x_ = value.x();
-    y_ = value.y();
-    z_ = value.z();
-    sync();
-  }
-
-  inline void operator=(const cgal::Point& value) {
-    x_ = value.x();
-    y_ = value.y();
-    z_ = value.z();
+  inline void operator=(const typename VectorAdap<N>::cgalPoint& value) {
+    this->toData(value, this->data_);
     sync();
   }
 
   inline void dummy() final {}
-  inline ~Vector3Out() {
+  inline ~VectorOut() {
     // write data to pointers before becoming out of scope.
     sync();
   }
@@ -120,23 +184,21 @@ class Vector3Out : public Vector3Adap {
  private:
   inline void sync() {
     if (external_eigen_ != nullptr) {
-      external_eigen_->x() = x_;
-      external_eigen_->y() = y_;
-      external_eigen_->z() = z_;
+      this->fromData(this->data_, external_eigen_);
     }
 
     if (external_cgal_ != nullptr) {
-      *external_cgal_ = cgal::Vector(x_, y_, z_);
+      this->fromData(this->data_, external_cgal_);
     }
     if (external_point_ != nullptr) {
-      *external_point_ = cgal::Point(x_, y_, z_);
+      this->fromData(this->data_, external_point_);
     }
   }
 
   // Pointers to external data.
-  Eigen::Vector3d* external_eigen_;
-  cgal::Vector* external_cgal_;
-  cgal::Point* external_point_;
+  typename VectorAdap<N>::eigenVector* external_eigen_;
+  typename VectorAdap<N>::cgalVector* external_cgal_;
+  typename VectorAdap<N>::cgalPoint* external_point_;
 };
 
 /*
@@ -145,24 +207,45 @@ class Vector3Out : public Vector3Adap {
  *
  * and supports casting to any 3 of these.
  */
-class Vector3In : public Vector3Adap {
+template <int N>
+class VectorIn : public VectorAdap<N> {
  public:
-  inline Vector3In(cgal::Vector vec) : Vector3Adap(vec) {}
-  inline Vector3In(Eigen::Vector3d eigen) : Vector3Adap(eigen) {}
-  inline Vector3In(cgal::Point pt) : Vector3Adap(pt) {}
+  inline VectorIn(cgal::Vector vec) : VectorAdap<N>(vec) {}
+  inline VectorIn(Eigen::Vector3d eigen) : VectorAdap<N>(eigen) {}
+  inline VectorIn(cgal::Point pt) : VectorAdap<N>(pt) {}
 
   // Disable assignment of Vector3In itself
-  void operator=(const Vector3In&) = delete;
+  void operator=(const VectorIn&) = delete;
 
   // Casts to eigen/cgal.
-  inline operator cgal::Vector() { return {x_, y_, z_}; }
-  inline operator Eigen::Vector3d() { return {x_, y_, z_}; }
-  inline operator cgal::Point() { return {x_, y_, z_}; }
+  inline operator cgal::Vector() {
+    cgal::Vector temp;
+    this->fromData(this->data_, &temp);
+    return temp;
+  }
+
+  inline operator Eigen::Vector3d() {
+    Eigen::Vector3d temp;
+    this->fromData(this->data_, &temp);
+    return temp;
+  }
+  inline operator cgal::Point() {
+    cgal::Point temp;
+    this->fromData(this->data_, &temp);
+    return temp;
+  }
 
  private:
   inline void dummy() final {}
 };
 
+// Typedefs for 2/3d
+typedef VectorOut<2> Vector2Out;
+typedef VectorIn<2> Vector2In;
+typedef Vector2In Vector2Return;
+
+typedef VectorOut<3> Vector3Out;
+typedef VectorIn<3> Vector3In;
 typedef Vector3In Vector3Return;
 
 }  // namespace cgal
