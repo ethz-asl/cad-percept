899dba24296e4a26205c31022a8ae5966a2ca300
Hermann <blumh@ethz.ch>
2019-09-16 18:47:50 +0200

    move a mesh with an interactive marker

--- cpt_utils/setup.py
+++ cpt_utils/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['cpt_meshcad_interface'],
+    packages=['cpt_utils'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
---
+++ cpt_utils/src/position_mesh_with_marker.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Transform, Vector3, Quaternion
+from visualization_msgs.msg import InteractiveMarkerControl
+from tf.broadcaster import TransformBroadcaster
+from std_srvs.srv import Empty, EmptyRequest
+
+from cgal_msgs.srv import PublishMesh
+from cpt_utils.marker import RosMarker
+
+
+def frame_callback(marker_msg, tf_broadcaster, update_mesh, map_frame):
+    time = rospy.Time.now()
+    pose = marker_msg.pose
+    tf_broadcaster.sendTransform(
+        (pose.position.x, pose.position.y, pose.position.z),
+        (pose.orientation.x, pose.orientation.y, pose.orientation.z,
+         pose.orientation.w),
+        time, 'marker_position', map_frame)
+    update_mesh('')
+
+
+if __name__ == "__main__":
+    rospy.init_node("mesh_postitioning")
+    transform_pub = rospy.Publisher('/T_map_marker', Transform, queue_size=10)
+    tf_broadcaster = TransformBroadcaster()
+
+    marker = RosMarker('BuildingModel',
+                       'Position this marker to initialize\n'
+                       'the position of the building model.',
+                       'interactive_mesh_marker_controls',
+                       InteractiveMarkerControl.MOVE_ROTATE_3D, show_6dof=True,
+                       parent_frame='world')
+
+    update_mesh = rospy.ServiceProxy('/mesh_publisher/publish', PublishMesh)
+
+    # Create a timer to update the published transforms
+    rospy.Timer(rospy.Duration(0.01), lambda msg: frame_callback(
+        marker.marker, tf_broadcaster, update_mesh, 'world'))
+
+    rospy.spin()
