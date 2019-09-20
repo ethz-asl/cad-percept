072dca5e6773290a6b69d016b15a9cfdfb65e0ab
Hermann <blumh@ethz.ch>
2019-09-20 17:25:28 +0200

    fix code style

--- relative_deviations/include/relative_deviations/deviations.h
+++ relative_deviations/include/relative_deviations/deviations.h
@@ -131,7 +131,7 @@ class Deviations {
   void detectMapChanges(std::vector<reconstructed_plane> *rec_planes, const PointCloud &map_cloud,
                         std::vector<reconstructed_plane> *remaining_plane_cloud_vector,
                         std::unordered_map<int, transformation> *current_transformation_map);
-  void init(cgal::Polyhedron &P, const tf::StampedTransform& transform);
+  void init(cgal::Polyhedron &P, const tf::StampedTransform &transform);
   std::unordered_map<int, polyhedron_plane>
       plane_map;  // plane map saving the ID of coplanar plane associated to plane properties
   std::unordered_map<int, transformation>
