684f253f6ad584542b270f87114b43fe743659ff
johnny <abel.gawel@mavt.ethz.ch>
2019-09-27 10:31:07 +0200

    Autoformatting.

--- cpt_deviation_analysis/include/cpt_deviation_analysis/relative_deviations_ros.h
+++ cpt_deviation_analysis/include/cpt_deviation_analysis/relative_deviations_ros.h
@@ -45,7 +45,7 @@ class RelativeDeviations {
   tf::TransformListener tf_listener_;
   std::ofstream timingFile;
   void publishMesh(const cgal::MeshModel::Ptr &model, ros::Publisher *publisher) const;
-  template <class T>
+  template<class T>
   void publishCloud(T *cloud, ros::Publisher *publisher) const;
   ros::Publisher buffer_pc_pub_, reconstructed_planes_pub_, polygon_pub_, assoc_mesh_pub_,
       assoc_pc_pub_, assoc_marker_pub_, bboxes_marker_pub_, deviations_mesh_pub_,
--- cpt_deviation_analysis/src/relative_deviations_ros.cpp
+++ cpt_deviation_analysis/src/relative_deviations_ros.cpp
@@ -156,7 +156,7 @@ void RelativeDeviations::processCloud(PointCloud &reading_pc) {
     cpt_utils::toRosTransform(transform.translation, transform.quat, &
         (deviation_msg.deviation_transform));
     pcl::toROSMsg(deviations.plane_map[plane_id].rec_plane.pointcloud, deviation_msg
-    .pointcloud);
+        .pointcloud);
     deviation_msg.pointcloud.header.frame_id = map_frame_;
     deviation_msg.pointcloud.header.stamp = ros::Time(0);
     deviations_pub_.publish(deviation_msg);
@@ -218,7 +218,7 @@ void RelativeDeviations::publishMesh(const cgal::MeshModel::Ptr &model,
   publisher->publish(c_msg);
 }
 
-template <class T>
+template<class T>
 void RelativeDeviations::publishCloud(T *cloud, ros::Publisher *publisher) const {
   cloud->header.frame_id = map_frame_;
   pcl_conversions::toPCL(ros::Time(0), cloud->header.stamp);
@@ -238,9 +238,9 @@ void RelativeDeviations::publishPolyhedron(cgal::Polyhedron &P) {
 
     do {
       geometry_msgs::Point32 p;
-      p.x = (float)(hit->vertex()->point()).x();
-      p.y = (float)(hit->vertex()->point()).y();
-      p.z = (float)(hit->vertex()->point()).z();
+      p.x = (float) (hit->vertex()->point()).x();
+      p.y = (float) (hit->vertex()->point()).y();
+      p.z = (float) (hit->vertex()->point()).z();
       msg.polygon.points.push_back(p);
     } while (++hit != j->facet_begin());
 
@@ -255,7 +255,7 @@ void RelativeDeviations::publishReconstructedPlanes(
     ColoredPointCloud pointcloud_plane_rgb;
     pcl::copyPointCloud(plane.pointcloud, pointcloud_plane_rgb);
     uint8_t r = std::rand() % 256, g = std::rand() % 256, b = std::rand() % 256;
-    uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b);
+    uint32_t rgb = ((uint32_t) r << 16 | (uint32_t) g << 8 | (uint32_t) b);
     for (uint i = 0; i < pointcloud_plane_rgb.points.size(); ++i) {
       pointcloud_plane_rgb.points[i].rgb = *reinterpret_cast<float *>(&rgb);
     }
@@ -285,7 +285,7 @@ void RelativeDeviations::publishAssociations(
   for (auto plane : remaining_plane_cloud_vector) {
     pcl::copyPointCloud(plane.pointcloud, pointcloud_plane_rgb);
     uint8_t r = 0, g = 0, b = 255;
-    uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b);
+    uint32_t rgb = ((uint32_t) r << 16 | (uint32_t) g << 8 | (uint32_t) b);
     for (uint i = 0; i < pointcloud_plane_rgb.points.size(); ++i) {
       pointcloud_plane_rgb.points[i].rgb = *reinterpret_cast<float *>(&rgb);
     }
@@ -334,7 +334,7 @@ void RelativeDeviations::publishAssociations(
       }
 
       pcl::copyPointCloud(plane.rec_plane.pointcloud, pointcloud_plane_rgb);
-      uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b);
+      uint32_t rgb = ((uint32_t) r << 16 | (uint32_t) g << 8 | (uint32_t) b);
       for (uint i = 0; i < pointcloud_plane_rgb.points.size(); ++i) {
         pointcloud_plane_rgb.points[i].rgb = *reinterpret_cast<float *>(&rgb);
       }
