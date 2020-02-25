49981e1f7dcace2f6959d39a1e66013dfffcc563
dreherm-student-ethz <dreherm@student.ethz.ch>
2020-02-25 20:07:43 +0100

    Added template code (not finished yet)

---
+++ matching_algorithms/include/test_Matcher/test_Matcher.h
@@ -0,0 +1,67 @@
+#ifndef TEST_MATCHER_H_
+#define TEST_MATCHER_H_
+
+#include <cgal_conversions/mesh_conversions.h>
+#include <cgal_conversions/eigen_conversions.h>
+#include <cgal_definitions/mesh_model.h>
+#include <cgal_msgs/TriangleMeshStamped.h>
+#include <cpt_utils/pc_processing.h>
+#include <pcl/point_types.h>
+#include <pointmatcher/PointMatcher.h>
+#include <pointmatcher_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+#include "test_Matcher/test_Matcher_parameters.h"
+
+namespace cad_percept {
+namespace cpt_matching_algorithms {
+
+typedef PointMatcher<float> PM;
+typedef PM::DataPoints DP;
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+
+class test_Matcher {
+ public:
+ 	test_Matcher(ros::NodeHandle &nh, ros::NodeHandle &nh_private);
+ private:
+ 	ros::NodeHandle &nh_, &nh_private_;
+ 	tf::StampedTransform transform;
+ 	Eigen::Matrix3d rotation;
+ 	Eigen::Vector3d translation;
+ 	Eigen::Matrix4d transformation;
+ 	cgal::Transformation ctransformation;
+ 	tf::TransformListener tf_listener_;
+ 	TestMatcherParameters parameters_;
+
+/**
+* Reference mesh for localization
+*/
+ 	cgal::MeshModel::Ptr reference_mesh_;
+
+//Point cloud variables
+    DP ref_dp;
+
+//Subscribers
+    ros::Subscriber cad_sub_;
+
+//Publisher
+    ros::Publisher scan_pub_;
+    ros::Publisher map_pub_;
+
+/**
+ * Mesh model callback
+*/
+    void gotCAD(const cgal_msgs::TriangleMeshStamped &cad_mesh_in);
+
+/**
+* Sample a point cloud from selected triangles of the mesh model
+*/
+    void sampleFromReferenceFacets(const int density, PointCloud *pointcloud);
+};
+
+}
+}
+
+#endif
---
+++ matching_algorithms/src/test_Matcher/test_Matcher_node.cpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+
+#include "test_Matcher/test_Matcher.h"
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "matcher_test");
+	ros::NodeHandle nh;
+	ros::NodeHandle nh_private("~");
+	//ROS_INFO_STREAM("Node is intialized");
+	cad_percept::cpt_matching_algorithms::test_Matcher test_Matcher(nh, nh_private);
+	//ROS_INFO_STREAM("Matcher is running")
+	ros::spin();
+
+	return 0;	
+}
