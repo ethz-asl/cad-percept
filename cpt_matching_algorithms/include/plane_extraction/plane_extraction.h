1fc3ce708d5b84b73bd93a75542678764cb78e87
dreherm-student-ethz <dreherm@student.ethz.ch>
2020-04-07 18:06:53 +0200

    add changes corresponding to feedback

---
+++ cpt_matching_algorithms/include/plane_extraction/plane_extraction.h
@@ -0,0 +1,63 @@
+#ifndef PLANE_EXTRACTION_H_
+#define PLANE_EXTRACTION_H_
+
+#include <cgal_conversions/eigen_conversions.h>
+#include <cgal_conversions/mesh_conversions.h>
+#include <cgal_definitions/mesh_model.h>
+#include <cgal_msgs/TriangleMeshStamped.h>
+#include <cpt_utils/pc_processing.h>
+#include <pcl/point_types.h>
+#include <pointmatcher/PointMatcher.h>
+#include <pointmatcher_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+#include <pcl/common/geometry.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+namespace cad_percept {
+namespace matching_algorithms {
+
+class PlaneExtractor {
+ public:
+  static void rhtPlaneExtraction(std::vector<pcl::PointCloud<pcl::PointXYZ>> &extracted_planes,
+                                 std::vector<std::vector<double>> &plane_coefficients,
+                                 pcl::PointCloud<pcl::PointXYZ> lidar_scan,
+                                 std::string tf_map_frame);
+  static void iterRhtPlaneExtraction(std::vector<pcl::PointCloud<pcl::PointXYZ>> &extracted_planes,
+                                     std::vector<std::vector<double>> &plane_coefficients,
+                                     pcl::PointCloud<pcl::PointXYZ> lidar_scan,
+                                     std::string tf_map_frame);
+  static void pclPlaneExtraction(std::vector<pcl::PointCloud<pcl::PointXYZ>> &extracted_planes,
+                                 std::vector<std::vector<double>> &plane_coefficients,
+                                 pcl::PointCloud<pcl::PointXYZ> lidar_scan,
+                                 std::string tf_map_frame);
+  static void visualizePlane(std::vector<pcl::PointCloud<pcl::PointXYZ>> &extracted_planes,
+                             ros::Publisher &plane_pub, std::string tf_map_frame);
+
+ private:
+  class HoughAccumulator;
+  class ArrayAccumulator;
+  class BallAccumulator;
+
+  static void rhtVote(int max_iteration, double tol_distance_between_points,
+                      double min_area_spanned, pcl::PointCloud<pcl::PointXYZ> lidar_scan,
+                      HoughAccumulator *accumulator);
+  static std::vector<std::vector<int>> rhtEval(
+      int num_main_planes, int min_vote_threshold, int k_of_maxima_suppression,
+      std::vector<std::vector<double>> &plane_coefficients,
+      std::vector<pcl::PointCloud<pcl::PointXYZ>> &extracted_planes,
+      const pcl::PointCloud<pcl::PointXYZ> lidar_scan, HoughAccumulator *accumulator);
+};
+
+}  // namespace matching_algorithms
+}  // namespace cad_percept
+
+#endif
---
+++ cpt_matching_algorithms/test/test_plane_extraction.cpp
@@ -0,0 +1,66 @@
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+#include <cgal_definitions/cgal_typedefs.h>
+#include <cgal_definitions/mesh_model.h>
+#include <cpt_utils/pc_processing.h>
+
+#include <plane_extraction/plane_extraction.h>
+
+using namespace cad_percept::cgal;
+using namespace cad_percept::matching_algorithms;
+
+// A modifier creating a triangle with the incremental builder.
+template <class HDS>
+class TestingMesh : public CGAL::Modifier_base<HDS> {
+ public:
+  TestingMesh() {}
+  void operator()(HDS& hds) {
+    // Create a simple plane parallel to y - z - plane at x =1
+    CGAL::Polyhedron_incremental_builder_3<HDS> B(hds, true);
+    B.begin_surface(4, 3);
+    B.add_vertex(Point(10, 0, 0));
+    B.add_vertex(Point(10, 10, 0));
+    B.add_vertex(Point(10, 0, 10));
+
+    B.begin_facet();
+    B.add_vertex_to_facet(0);
+    B.add_vertex_to_facet(1);
+    B.add_vertex_to_facet(2);
+    B.end_facet();
+
+    B.end_surface();
+  }
+};
+
+TEST(RHTExtractionTest, normal_computation_testCase1) {
+  Polyhedron m;
+  TestingMesh<HalfedgeDS> testcase;
+  m.delegate(testcase);
+
+  MeshModel::Ptr model;
+  MeshModel::create(m, &model);
+
+  pcl::PointCloud<pcl::PointXYZ> pointcloud;
+  cad_percept::cpt_utils::sample_pc_from_mesh(model->getMesh(), 100, 0.0, &pointcloud);
+
+  std::vector<pcl::PointCloud<pcl::PointXYZ>> extracted_planes;
+  std::vector<std::vector<double>> plane_coefficients;
+  std::string tf_map_frame = "map";
+
+  PlaneExtractor::pclPlaneExtraction(extracted_planes, plane_coefficients, pointcloud,
+                                     tf_map_frame);
+  EXPECT_TRUE(std::abs(plane_coefficients[0][0] - 1) < 0.5 &&
+              std::abs(plane_coefficients[0][1] - 0) < 0.2 &&
+              std::abs(plane_coefficients[0][2] - 0) < 0.2);
+  PlaneExtractor::rhtPlaneExtraction(extracted_planes, plane_coefficients, pointcloud,
+                                     tf_map_frame);
+  EXPECT_TRUE(std::abs(plane_coefficients[0][0] - 1) < 0.5 &&
+              std::abs(plane_coefficients[0][1] - 0) < 0.2 &&
+              std::abs(plane_coefficients[0][2] - 0) < 0.2);
+  PlaneExtractor::iterRhtPlaneExtraction(extracted_planes, plane_coefficients, pointcloud,
+                                         tf_map_frame);
+  EXPECT_TRUE(std::abs(plane_coefficients[0][0] - 1) < 0.5 &&
+              std::abs(plane_coefficients[0][1] - 0) < 0.2 &&
+              std::abs(plane_coefficients[0][2] - 0) < 0.2);
+}
