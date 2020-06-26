32858c20548491b37e319eb284023a4dd3f4d551
dreherm-student-ethz <dreherm@student.ethz.ch>
2020-06-26 23:19:36 +0200

    add working goicp demo (allows correction of pre-processing)

--- cpt_matching_algorithms/src/test_matcher/matcher/go_icp_matcher.cpp
+++ cpt_matching_algorithms/src/test_matcher/matcher/go_icp_matcher.cpp
@@ -134,7 +134,7 @@ void TestMatcher::goicpMatch() {
   }
 
   // Get matrix of unscaled matrix
-  Eigen::Matrix4d final_transf = go_icp_trans.inverse();
+  Eigen::Matrix4d final_transf = go_icp_trans;
 
   Eigen::Matrix3d final_rot = final_transf.block(0, 0, 3, 3);
   Eigen::Quaterniond final_q(final_rot);
