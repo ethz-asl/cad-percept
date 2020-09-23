34eef72135c07f40cc2e23fbba29407f78817c0c
michaelpantic <michael.pantic@gmail.com>
2020-09-24 00:52:04 +0200

    comparison rrt vs rmp

--- modules/planning/cpt_ompl_planning/include/cpt_ompl_planning/ompl_mesh_sampling_planner.h
+++ modules/planning/cpt_ompl_planning/include/cpt_ompl_planning/ompl_mesh_sampling_planner.h
@@ -1,6 +1,9 @@
 
 #ifndef CPT_OMPL_PLANNING_OMPL_MESH_SAMPLING_PLANNER_H
 #define CPT_OMPL_PLANNING_OMPL_MESH_SAMPLING_PLANNER_H
+#include <CGAL/point_generators_3.h>
+#include <cgal_definitions/cgal_typedefs.h>
+#include <cgal_definitions/mesh_model.h>
 #include <ompl/base/objectives/PathLengthOptimizationObjective.h>
 #include <ompl/base/spaces/SE3StateSpace.h>
 #include <ompl/geometric/SimpleSetup.h>
@@ -9,114 +12,139 @@
 #include <ompl/geometric/planners/rrt/InformedRRTstar.h>
 #include <ompl/geometric/planners/rrt/RRTConnect.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
+#include <ros/ros.h>
+#include <visualization_msgs/MarkerArray.h>
 
 #include <iostream>
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
-// This is a problem-specific sampler that automatically generates valid
-// states; it doesn't need to call SpaceInformation::isValid. This is an
-// example of constrained sampling. If you can explicitly describe the set valid
-// states and can draw samples from it, then this is typically much more
-// efficient than generating random samples from the entire state space and
-// checking for validity.
-
-class MeshManifoldSampler : public ob::ValidStateSampler {
+class MeshManifoldSampler : public ob::StateSampler {
  public:
-  MeshManifoldSampler(const ob::SpaceInformation* si) : ValidStateSampler(si) {
-    name_ = "my sampler";
-  }
-  // Generate a sample in the valid part of the R^3 state space
-  // Valid states satisfy the following constraints:
-  // -1<= x,y,z <=1
-  // if .25 <= z <= .5, then |x| >.8 and |y| >.8
-  bool sample(ob::State* state) override {
-    double* val = static_cast<ob::RealVectorStateSpace::StateType*>(state)->values;
-    double z = rng_.uniformReal(-1, 1);
+  MeshManifoldSampler(const ob::StateSpace* space, cad_percept::cgal::MeshModel::Ptr model)
+      : StateSampler(space), model_(model) {
+    model_->getArea();
 
-    if (z > .25 && z < .5) {
-      double x = rng_.uniformReal(0, 1.8), y = rng_.uniformReal(0, .2);
-      switch (rng_.uniformInt(0, 3)) {
-        case 0:
-          val[0] = x - 1;
-          val[1] = y - 1;
-          break;
-        case 1:
-          val[0] = x - .8;
-          val[1] = y + .8;
-          break;
-        case 2:
-          val[0] = y - 1;
-          val[1] = x - 1;
-          break;
-        case 3:
-          val[0] = y + .8;
-          val[1] = x - .8;
-          break;
-      }
-    } else {
-      val[0] = rng_.uniformReal(-1, 1);
-      val[1] = rng_.uniformReal(-1, 1);
-    }
-    val[2] = z;
-    assert(si_->isValid(state));
-    return true;
+    rng_mesh_ =
+        std::make_shared<CGAL::Random_points_in_triangle_mesh_3<cad_percept::cgal::Polyhedron>>(
+            model->getMeshRef());
+  }
+
+  void sampleUniform(ob::State* state) override {
+    double* val = static_cast<ob::RealVectorStateSpace::StateType*>(state)->values;
+    // set val[0],val[1]
+    std::vector<cad_percept::cgal::Point> points;
+
+    std::copy_n(*rng_mesh_, 1, std::back_inserter(points));
+    rng_mesh_->operator++();
+
+    val[0] = points[0].x();
+    val[1] = points[0].y();
+    val[2] = points[0].z();
   }
   // We don't need this in the example below.
-  bool sampleNear(ob::State* /*state*/, const ob::State* /*near*/,
-                  const double /*distance*/) override {
+  void sampleUniformNear(ob::State* /*state*/, const ob::State* /*near*/,
+                         const double /*distance*/) override {
     throw ompl::Exception("MyValidStateSampler::sampleNear", "not implemented");
-    return false;
+  }
+
+  void sampleGaussian(ob::State* state, const ob::State* mean, double stdDev) override {
+    throw ompl::Exception("MyValidStateSampler::sampleGaussian", "not implemented");
   }
 
  protected:
-  ompl::RNG rng_;
+  cad_percept::cgal::MeshModel::Ptr model_;
+  std::shared_ptr<CGAL::Random_points_in_triangle_mesh_3<cad_percept::cgal::Polyhedron>> rng_mesh_;
 };
 
 class OMPLMeshSamplingPlanner {
  public:
-  static ob::ValidStateSamplerPtr allocMeshManifoldSampler(const ob::SpaceInformation* si) {
-    return std::make_shared<MeshManifoldSampler>(si);
+  OMPLMeshSamplingPlanner(bool connect) : rrt_connect_(connect) {
+    std::cout << cad_percept::cgal::MeshModel::create(
+                     "/home/mpantic/ws/rmp/src/manifold_simulations/models/hilo_roof/meshes/"
+                     "hilo_reconstructed.off",
+                     &model_, true)
+              << std::endl;
+
+    ros::NodeHandle node_handle;
   }
 
-  void plan() {
+  ob::StateSamplerPtr allocMeshManifoldSampler(const ob::StateSpace* space) {
+    return std::make_shared<MeshManifoldSampler>(space, model_);
+  }
+
+  bool meshStateValidityChecker(const ob::State* state) {
+    const double* val = static_cast<const ob::RealVectorStateSpace::StateType*>(state)->values;
+
+    cad_percept::cgal::Point pt(val[0], val[1], val[2]);
+
+    return (model_->squaredDistance(pt) < 0.01 * 0.01);
+  }
+
+  bool plan(Eigen::Vector3d startpoint, Eigen::Vector3d endpoint,
+            std::vector<Eigen::Vector3d>* pathpoints) {
     std::cout << "Plantest2" << std::endl;
-    ob::StateSpacePtr space(new ob::RealVectorStateSpace(2));
-    space->as<ob::RealVectorStateSpace>()->setBounds(0.0, 1.0);
+    ob::StateSpacePtr space(new ob::RealVectorStateSpace(3));
+    space->as<ob::RealVectorStateSpace>()->setBounds(-23.0, 23.0);
 
     ob::SpaceInformationPtr si(new ob::SpaceInformation(space));
+
+    si->setStateValidityChecker(
+        std::bind(&OMPLMeshSamplingPlanner::meshStateValidityChecker, this, std::placeholders::_1));
+    si->setStateValidityCheckingResolution(0.01);  // 1%
+
     auto ss = std::make_shared<og::SimpleSetup>(si);
-
-    ob::PlannerPtr optimizingPlanner(new og::RRTstar(si));
-
+    space->setStateSamplerAllocator(
+        std::bind(&OMPLMeshSamplingPlanner::allocMeshManifoldSampler, this, std::placeholders::_1));
     // set up solver
+
+    ob::PlannerPtr optimizingPlanner;
+    if (rrt_connect_) {
+      optimizingPlanner.reset(new og::RRTConnect(si));
+    } else {
+      optimizingPlanner.reset(new og::RRTstar(si));
+    }
+
+    optimizingPlanner->setup();
+
     ss->setPlanner(optimizingPlanner);
-    ss->getSpaceInformation()->setValidStateSamplerAllocator(this->allocMeshManifoldSampler);
 
     // set start & goal
     ob::ScopedState<> start(space);
-    start->as<ob::RealVectorStateSpace::StateType>()->values[0] = 0.0;
-    start->as<ob::RealVectorStateSpace::StateType>()->values[1] = 0.0;
+    start->as<ob::RealVectorStateSpace::StateType>()->values[0] = startpoint.x();
+    start->as<ob::RealVectorStateSpace::StateType>()->values[1] = startpoint.y();
+    start->as<ob::RealVectorStateSpace::StateType>()->values[2] = startpoint.z();
 
     ob::ScopedState<> goal(space);
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[0] = 1.0;
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[1] = 1.0;
+    goal->as<ob::RealVectorStateSpace::StateType>()->values[0] = endpoint.x();
+    goal->as<ob::RealVectorStateSpace::StateType>()->values[1] = endpoint.y();
+    goal->as<ob::RealVectorStateSpace::StateType>()->values[2] = endpoint.z();
 
     ss->setStartAndGoalStates(start, goal);
-    ob::PlannerStatus solved = ss->solve(1.0);
-    std::cout << solved << std::endl;
+
+    std::cout << "Starting solve" << std::endl;
+    ob::PlannerStatus solved = ss->solve(solve_time_);
+     if (solved != ob::PlannerStatus::EXACT_SOLUTION) {
+      return false;
+    }
 
     // get path
     ompl::geometric::PathGeometric path = ss->getSolutionPath();
-    std::cout << "Length: " << path.length() << std::endl;
-    std::cout << "Elems: " << path.getStateCount() << std::endl;
 
     for (auto state : path.getStates()) {
-      std::cout << state->as<ob::RealVectorStateSpace::StateType>()->values[0] << " "
-                << state->as<ob::RealVectorStateSpace::StateType>()->values[0] << std::endl;
+      Eigen::Vector3d pt;
+      pt.x() = state->as<ob::RealVectorStateSpace::StateType>()->values[0];
+      pt.y() = state->as<ob::RealVectorStateSpace::StateType>()->values[1];
+      pt.z() = state->as<ob::RealVectorStateSpace::StateType>()->values[2];
+      pathpoints->push_back(pt);
     }
+    return true;
   }
+
+  double solve_time_{1.0};
+  bool rrt_connect_{false};
+  cad_percept::cgal::MeshModel::Ptr model_;
 };
 
 #endif  // CPT_OMPL_PLANNING_OMPL_MESH_SAMPLING_PLANNER_H
--- modules/planning/cpt_ompl_planning/src/ompl_mesh_sampling_planner.cpp
+++ modules/planning/cpt_ompl_planning/src/ompl_mesh_sampling_planner.cpp
@@ -1,8 +1,131 @@
+#include <CGAL/point_generators_3.h>
 #include <cpt_ompl_planning/ompl_mesh_sampling_planner.h>
+#include <cpt_ompl_planning/rmp_mesh_planner.h>
+#include <cpt_ros/mesh_model_publisher.h>
 
-int main() {
-  OMPLMeshSamplingPlanner planner;
-  planner.plan();
+#include <chrono>
 
+ros::Publisher vis_pub_;
+
+void visualizePath(std::vector<Eigen::Vector3d> path, Eigen::Vector3d color, int id) {
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = "world";
+  marker.header.stamp = ros::Time();
+  marker.ns = "path";
+  marker.id = id;
+  marker.type = visualization_msgs::Marker::LINE_STRIP;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = 0;
+  marker.pose.position.y = 0;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+  marker.scale.x = 0.1;
+  marker.scale.y = 0.0;
+  marker.scale.z = 0.0;
+  marker.color.a = 1.0;  // Don't forget to set the alpha!
+  marker.color.r = color.x();
+  marker.color.g = color.y();
+  marker.color.b = color.z();
+
+  for (auto state : path) {
+    geometry_msgs::Point pt;
+    pt.x = state.x();
+    pt.y = state.y();
+    pt.z = state.z();
+    marker.points.push_back(pt);
+  }
+
+  vis_pub_.publish(marker);
+}
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "ompl_test_node");
+  ros::NodeHandle node_handle;
+  cad_percept::cgal::MeshModel::Ptr model;
+  cad_percept::cgal::MeshModel::create(
+      "/home/mpantic/ws/rmp/src/manifold_simulations/models/hilo_roof/meshes/"
+      "hilo_reconstructed.off",
+      &model, true);
+  cad_percept::MeshModelPublisher pub_mesh_3d_(node_handle, "mesh");
+  pub_mesh_3d_.publish(model);
+
+  auto rng_mesh_ =
+      std::make_shared<CGAL::Random_points_in_triangle_mesh_3<cad_percept::cgal::Polyhedron>>(
+          model->getMeshRef());
+
+  vis_pub_ = node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 0);
+  RMPMeshPlanner rmp_planner;
+  OMPLMeshSamplingPlanner ompl_planner(false);
+  OMPLMeshSamplingPlanner ompl_planner_connect(true);
+
+  while (ros::ok()) {
+    std::vector<cad_percept::cgal::Point> points;
+    std::copy_n(*rng_mesh_, 3, std::back_inserter(points));
+    Eigen::Vector3d startpoint, endpoint;
+    startpoint.x() = points[1].x();
+    startpoint.y() = points[1].y();
+    startpoint.z() = points[1].z();
+    endpoint.x() = points[2].x();
+    endpoint.y() = points[2].y();
+    endpoint.z() = points[2].z();
+
+    /*  startpoint.x() = 1.568;
+      startpoint.y() = 3.6077;
+      startpoint.z() = 4.29022;
+      endpoint.x() = 17.1972;
+      endpoint.y() = 5.37636;
+      endpoint.z() = 5.13425;*/
+
+    std::vector<Eigen::Vector3d> path_rrt_star;
+
+    std::chrono::steady_clock::time_point begin_rrt_star = std::chrono::steady_clock::now();
+    bool rrt_star_success = ompl_planner.plan(startpoint, endpoint, &path_rrt_star);
+    std::chrono::steady_clock::time_point end_rrt_star = std::chrono::steady_clock::now();
+
+    visualizePath(path_rrt_star, {0.0, 1.0, 0.0}, 0);
+
+    std::vector<Eigen::Vector3d> path_rrt_connect;
+    std::chrono::steady_clock::time_point begin_rrt_connect = std::chrono::steady_clock::now();
+
+    bool rrt_connect_success = ompl_planner_connect.plan(startpoint, endpoint, &path_rrt_connect);
+    std::chrono::steady_clock::time_point end_rrt_connect = std::chrono::steady_clock::now();
+
+    visualizePath(path_rrt_connect, {0.0, 0.0, 1.0}, 1);
+
+    std::vector<Eigen::Vector3d> path_rmp;
+    std::chrono::steady_clock::time_point begin_rmp = std::chrono::steady_clock::now();
+    bool rmp_success =  rmp_planner.plan(startpoint, endpoint, &path_rmp);
+    std::chrono::steady_clock::time_point end_rmp = std::chrono::steady_clock::now();
+    visualizePath(path_rmp, {1.0, 0.0, 0.0}, 2);
+
+    std::cout << "RRT*\t =\t "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end_rrt_star -
+                                                                       begin_rrt_star)
+                     .count()
+              << "[µs]" << std::endl;
+    std::cout << "BiRRT\t =\t "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end_rrt_connect -
+                                                                       begin_rrt_connect)
+                     .count()
+              << "[µs]" << std::endl;
+    std::cout << "RMP\t =\t "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end_rmp - begin_rmp).count()
+              << "[µs]" << std::endl;
+
+    std::cout << "Data\t"
+              << (rrt_star_success ? std::chrono::duration_cast<std::chrono::microseconds>(end_rrt_star -
+                                                                       begin_rrt_star)
+                     .count() : -1)
+              << "\t"
+              << (rrt_connect_success ? std::chrono::duration_cast<std::chrono::microseconds>(end_rrt_connect -
+                                                                       begin_rrt_connect)
+                     .count() : -1)
+              << "\t"
+              << (rmp_success ? std::chrono::duration_cast<std::chrono::microseconds>(end_rmp - begin_rmp).count() : -1)
+              << std::endl;
+  }
   return 0;
 }
