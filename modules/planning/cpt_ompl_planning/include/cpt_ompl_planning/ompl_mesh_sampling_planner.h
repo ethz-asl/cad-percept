b86541959c4826b26b8c29c60c071e2ae0d2ed5c
michaelpantic <michael.pantic@gmail.com>
2020-09-24 21:06:48 +0200

    more refactoring

--- modules/planning/cpt_ompl_planning/include/cpt_ompl_planning/ompl_mesh_sampling_planner.h
+++ modules/planning/cpt_ompl_planning/include/cpt_ompl_planning/ompl_mesh_sampling_planner.h
@@ -4,6 +4,8 @@
 #include <CGAL/point_generators_3.h>
 #include <cgal_definitions/cgal_typedefs.h>
 #include <cgal_definitions/mesh_model.h>
+#include <cpt_planning/implementation/rmp_mesh_planner.h>
+#include <cpt_planning/interface/surface_planner.h>
 #include <ompl/base/objectives/PathLengthOptimizationObjective.h>
 #include <ompl/base/spaces/SE3StateSpace.h>
 #include <ompl/geometric/SimpleSetup.h>
@@ -12,7 +14,6 @@
 #include <ompl/geometric/planners/rrt/InformedRRTstar.h>
 #include <ompl/geometric/planners/rrt/RRTConnect.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
-#include <ros/ros.h>
 #include <visualization_msgs/MarkerArray.h>
 
 #include <iostream>
@@ -58,23 +59,15 @@ class MeshManifoldSampler : public ob::StateSampler {
   std::shared_ptr<CGAL::Random_points_in_triangle_mesh_3<cad_percept::cgal::Polyhedron>> rng_mesh_;
 };
 
-class OMPLMeshSamplingPlanner {
+class OMPLMeshSamplingPlanner : cad_percept::planning::SurfacePlanner {
  public:
-  OMPLMeshSamplingPlanner(bool connect) : rrt_connect_(connect) {
-    std::cout << cad_percept::cgal::MeshModel::create(
-                     "/home/mpantic/ws/rmp/src/manifold_simulations/models/hilo_roof/meshes/"
-                     "hilo_reconstructed.off",
-                     &model_, true)
-              << std::endl;
+  OMPLMeshSamplingPlanner(std::string meshpath, bool connect);
 
-    ros::NodeHandle node_handle;
-  }
-
-  ob::StateSamplerPtr allocMeshManifoldSampler(const ob::StateSpace* space) {
+  inline ob::StateSamplerPtr allocMeshManifoldSampler(const ob::StateSpace* space) {
     return std::make_shared<MeshManifoldSampler>(space, model_);
   }
 
-  bool meshStateValidityChecker(const ob::State* state) {
+  inline bool meshStateValidityChecker(const ob::State* state) {
     const double* val = static_cast<const ob::RealVectorStateSpace::StateType*>(state)->values;
 
     cad_percept::cgal::Point pt(val[0], val[1], val[2]);
@@ -82,65 +75,10 @@ class OMPLMeshSamplingPlanner {
     return (model_->squaredDistance(pt) < 0.01 * 0.01);
   }
 
-  bool plan(Eigen::Vector3d startpoint, Eigen::Vector3d endpoint,
-            std::vector<Eigen::Vector3d>* pathpoints) {
-    std::cout << "Plantest2" << std::endl;
-    ob::StateSpacePtr space(new ob::RealVectorStateSpace(3));
-    space->as<ob::RealVectorStateSpace>()->setBounds(-23.0, 23.0);
+  inline const std::string getName() const { return rrt_connect_ ?  "RRTConnect" : "RRTStar"; }
 
-    ob::SpaceInformationPtr si(new ob::SpaceInformation(space));
-
-    si->setStateValidityChecker(
-        std::bind(&OMPLMeshSamplingPlanner::meshStateValidityChecker, this, std::placeholders::_1));
-    si->setStateValidityCheckingResolution(0.01);  // 1%
-
-    auto ss = std::make_shared<og::SimpleSetup>(si);
-    space->setStateSamplerAllocator(
-        std::bind(&OMPLMeshSamplingPlanner::allocMeshManifoldSampler, this, std::placeholders::_1));
-    // set up solver
-
-    ob::PlannerPtr optimizingPlanner;
-    if (rrt_connect_) {
-      optimizingPlanner.reset(new og::RRTConnect(si));
-    } else {
-      optimizingPlanner.reset(new og::RRTstar(si));
-    }
-
-    optimizingPlanner->setup();
-
-    ss->setPlanner(optimizingPlanner);
-
-    // set start & goal
-    ob::ScopedState<> start(space);
-    start->as<ob::RealVectorStateSpace::StateType>()->values[0] = startpoint.x();
-    start->as<ob::RealVectorStateSpace::StateType>()->values[1] = startpoint.y();
-    start->as<ob::RealVectorStateSpace::StateType>()->values[2] = startpoint.z();
-
-    ob::ScopedState<> goal(space);
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[0] = endpoint.x();
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[1] = endpoint.y();
-    goal->as<ob::RealVectorStateSpace::StateType>()->values[2] = endpoint.z();
-
-    ss->setStartAndGoalStates(start, goal);
-
-    std::cout << "Starting solve" << std::endl;
-    ob::PlannerStatus solved = ss->solve(solve_time_);
-     if (solved != ob::PlannerStatus::EXACT_SOLUTION) {
-      return false;
-    }
-
-    // get path
-    ompl::geometric::PathGeometric path = ss->getSolutionPath();
-
-    for (auto state : path.getStates()) {
-      Eigen::Vector3d pt;
-      pt.x() = state->as<ob::RealVectorStateSpace::StateType>()->values[0];
-      pt.y() = state->as<ob::RealVectorStateSpace::StateType>()->values[1];
-      pt.z() = state->as<ob::RealVectorStateSpace::StateType>()->values[2];
-      pathpoints->push_back(pt);
-    }
-    return true;
-  }
+  const SurfacePlanner::Result plan(const Eigen::Vector3d start, const Eigen::Vector3d goal,
+                                    std::vector<Eigen::Vector3d>* states_out);
 
   double solve_time_{1.0};
   bool rrt_connect_{false};
--- modules/planning/cpt_planning/include/cpt_planning/implementation/rmp_mesh_planner.h
+++ modules/planning/cpt_planning/include/cpt_planning/implementation/rmp_mesh_planner.h
@@ -5,28 +5,29 @@
 #include <cpt_planning/coordinates/face_coords.h>
 #include <cpt_planning/coordinates/uv_mapping.h>
 #include <cpt_planning/interface/mesh_manifold_interface.h>
+#include <cpt_planning/interface/surface_planner.h>
 #include <rmpcpp/core/space.h>
 #include <rmpcpp/core/state.h>
 #include <rmpcpp/eval/trapezoidal_integrator.h>
 #include <rmpcpp/policies/simple_target_policy.h>
-#include <cpt_planning/interface/surface_planner.h>
 
 #include <iostream>
 namespace cad_percept {
-    namespace planning {
+namespace planning {
 class RMPMeshPlanner : SurfacePlanner {
  public:
   RMPMeshPlanner(std::string mesh_path);
 
   const SurfacePlanner::Result plan(const Eigen::Vector3d start, const Eigen::Vector3d goal,
-            std::vector<Eigen::Vector3d> *states_out);
+                                    std::vector<Eigen::Vector3d> *states_out);
 
-    const std::string getName() const;
+  inline const std::string getName() const { return "RMP"; }
 
   cad_percept::cgal::MeshModel::Ptr model_;
   cad_percept::planning::UVMapping *mapping_;
   cad_percept::planning::MeshManifoldInterface *manifold_;
 };
-}}
+}  // namespace planning
+}  // namespace cad_percept
 
 #endif  // CPT_PLANNING_IMPLEMENTATION_RMP_MESH_PLANNER_H
