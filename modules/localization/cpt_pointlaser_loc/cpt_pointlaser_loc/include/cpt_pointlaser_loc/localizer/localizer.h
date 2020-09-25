c1bdaf970b68869a521318f520f49a2288140c32
Francesco Milano <francesco.milano172@gmail.com>
2020-09-25 18:11:50 +0200

    Start adding PointLaserLocalizer::localize

--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/localizer/localizer.h
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/localizer/localizer.h
@@ -26,6 +26,41 @@ class PointLaserLocalizer {
                       const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
                       double pointlaser_noise_std);
 
+  /// \brief Performs localization w.r.t. the mesh model.
+  /// TODO(fmilano): Check for exact meaning of poses.
+  ///
+  /// \param marker_to_armbase          Pose between the marker and the arm
+  ///                                   base.
+  /// \param initial_pose               Pose between the arm base and the Kinova
+  ///                                   link.
+  /// \param laser_a_offset             Pose between the Kinova link and the
+  ///                                   point laser A.
+  /// \param laser_b_offset             Pose between the Kinova link and the
+  ///                                   point laser B.
+  /// \param laser_c_offset             Pose between the Kinova link and the
+  ///                                   point laser C.
+  /// \param endeffector_offset         Pose between the Kinova link and the
+  ///                                   Kinova end effector.
+  /// \param arm_base_to_base           Pose between the arm base and the base.
+  /// \param fix_cad_planes             If True, the planes retrieved by the
+  ///                                   optimizer are fixed.
+  //  \param add_initial_pose_prior     If True, a factor for the prior of the
+  ///                                   model offset is added to the
+  ///                                   optimization graph.
+  /// \param only_optimize_translation  If True, only optimizes translation.
+  /// \return True if localization can be successfully performed, False
+  //          otherwise.
+  bool localize(const kindr::minimal::QuatTransformation& marker_to_armbase,
+                const kindr::minimal::QuatTransformation& initial_pose,
+                const kindr::minimal::QuatTransformation& laser_a_offset,
+                const kindr::minimal::QuatTransformation& laser_b_offset,
+                const kindr::minimal::QuatTransformation& laser_c_offset,
+                const kindr::minimal::QuatTransformation& endeffector_offset,
+                const kindr::minimal::QuatTransformation& arm_base_to_base,
+                bool fix_cad_planes = false,
+                bool add_initial_pose_prior = false,
+                bool only_optimize_translation = false);
+
  private:
   // Mesh model w.r.t. which localization should be performed.
   cad_percept::cgal::MeshModel::Ptr model_;
--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/localizer/localizer.cpp
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/localizer/localizer.cpp
@@ -15,6 +15,23 @@ PointLaserLocalizer::PointLaserLocalizer(
       odometry_noise_std_(odometry_noise_std),
       pointlaser_noise_std_(pointlaser_noise_std) {}
 
+bool PointLaserLocalizer::localize(
+    const kindr::minimal::QuatTransformation& marker_to_armbase,
+    const kindr::minimal::QuatTransformation& initial_pose,
+    const kindr::minimal::QuatTransformation& laser_a_offset,
+    const kindr::minimal::QuatTransformation& laser_b_offset,
+    const kindr::minimal::QuatTransformation& laser_c_offset,
+    const kindr::minimal::QuatTransformation& endeffector_offset,
+    const kindr::minimal::QuatTransformation& arm_base_to_base,
+    bool fix_cad_planes, bool add_initial_pose_prior,
+    bool only_optimize_translation) {
+  // NOTE: we assume that the arm was already moved to the initial pose.
+  // TODO(fmilano): Check!
+
+  // Instantiate an optimizer with the initial poses:
+  // -
+}
+
 }  // namespace localizer
 }  // namespace pointlaser_loc
 }  // namespace cad_percept
