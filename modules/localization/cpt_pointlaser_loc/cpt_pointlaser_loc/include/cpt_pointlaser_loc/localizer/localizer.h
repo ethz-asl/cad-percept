a3dce6d8285f42e5ffcc3e85d77424cc5f477010
Francesco Milano <francesco.milano172@gmail.com>
2020-09-25 17:18:01 +0200

    Implement constructor of PointLaserLocalizer In `cpt_pointlaser_loc/localizer` module.

--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/localizer/localizer.h
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/localizer/localizer.h
@@ -1,14 +1,28 @@
 #ifndef CPT_POINTLASER_LOC_LOCALIZER_LOCALIZER_H_
 #define CPT_POINTLASER_LOC_LOCALIZER_LOCALIZER_H_
 
+#include <cgal_definitions/mesh_model.h>
+
+#include <Eigen/Geometry>
+
 namespace cad_percept {
 namespace pointlaser_loc {
 namespace localizer {
+
+/// \brief Simple class to localize the laser sensor w.r.t. a mesh model.
 class PointLaserLocalizer {
  public:
-  PointLaserLocalizer();
+  PointLaserLocalizer(const cad_percept::cgal::MeshModel::Ptr& model,
+                      const Eigen::Matrix<double, 6, 1>& initial_pose_std,
+                      const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
+                      double pointlaser_noise_std);
 
  private:
+  // Mesh model w.r.t. which localization should be performed.
+  cad_percept::cgal::MeshModel::Ptr model_;
+  // Noise statistics.
+  Eigen::Matrix<double, 6, 1> initial_pose_std_, odometry_noise_std_;
+  double pointlaser_noise_std_;
 };
 }  // namespace localizer
 }  // namespace pointlaser_loc
--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/localizer/localizer.cpp
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/localizer/localizer.cpp
@@ -1,9 +1,19 @@
 #include "cpt_pointlaser_loc/localizer/localizer.h"
 
+#include "cpt_pointlaser_loc/optimizer/optimizer.h"
+
 namespace cad_percept {
 namespace pointlaser_loc {
 namespace localizer {
-PointLaserLocalizer::PointLaserLocalizer() {}
+PointLaserLocalizer::PointLaserLocalizer(
+    const cad_percept::cgal::MeshModel::Ptr& model,
+    const Eigen::Matrix<double, 6, 1>& initial_pose_std,
+    const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
+    double pointlaser_noise_std)
+    : model_(model),
+      initial_pose_std_(initial_pose_std),
+      odometry_noise_std_(odometry_noise_std),
+      pointlaser_noise_std_(pointlaser_noise_std) {}
 
 }  // namespace localizer
 }  // namespace pointlaser_loc
