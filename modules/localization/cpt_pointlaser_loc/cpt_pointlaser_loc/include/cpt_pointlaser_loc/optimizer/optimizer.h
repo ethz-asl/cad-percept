6eb91f5accc15188845379a97151830d77e91d27
Francesco Milano <francesco.milano172@gmail.com>
2020-09-25 15:29:10 +0200

    Replace `ArchitectModel` with `MeshModel`

--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/optimizer/optimizer.h
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/optimizer/optimizer.h
@@ -1,6 +1,7 @@
 #ifndef CPT_POINTLASER_LOC_OPTIMIZER_OPTIMIZER_H_
 #define CPT_POINTLASER_LOC_OPTIMIZER_OPTIMIZER_H_
 
+#include <cgal_definitions/mesh_model.h>
 #include <gtsam/linear/NoiseModel.h>
 #include <gtsam/nonlinear/ExpressionFactorGraph.h>
 #include <gtsam/nonlinear/Values.h>
@@ -9,8 +10,6 @@
 #include <Eigen/Geometry>
 
 #include "cpt_pointlaser_loc/optimizer/common.h"
-#include "pointlaser_loc/architectmodel.h"
-#include "pointlaser_loc/architectmodel_fake_trait.h"
 
 namespace cad_percept {
 namespace pointlaser_loc {
@@ -21,7 +20,7 @@ class LocalizationOptimizer {
   LocalizationOptimizer(
       const kindr::minimal::QuatTransformation& architecture_offset,
       const kindr::minimal::QuatTransformation& initial_pose,
-      const std::shared_ptr<architect_model::ArchitectModel> arch_model,
+      const cad_percept::cgal::MeshModel::Ptr& arch_model,
       const Eigen::Matrix<double, 6, 1>& architecture_offset_std,
       const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
       const double pointlaser_noise_std, const bool fix_retrieved_planes,
@@ -38,11 +37,12 @@ class LocalizationOptimizer {
   kindr::minimal::QuatTransformation current_arm_pose_,
       initial_architect_offset_;
   ETransformation architect_offset_;
-  std::shared_ptr<architect_model::ArchitectModel> architect_model_;
+  cad_percept::cgal::MeshModel::Ptr architect_model_;
   gtsam::noiseModel::Base::shared_ptr pointlaser_noise_;
   gtsam::noiseModel::Diagonal::shared_ptr odometry_noise_;
   gtsam::Values initialization_;
 };
+
 }  // namespace optimizer
 }  // namespace pointlaser_loc
 }  // namespace cad_percept
