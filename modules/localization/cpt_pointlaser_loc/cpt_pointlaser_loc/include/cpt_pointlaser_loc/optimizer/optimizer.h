4d269493d802afb6ec7c027dd0d117cc0c519a6e
Francesco Milano <francesco.milano172@gmail.com>
2020-10-01 15:20:21 +0200

    Fix: fit with style format

--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/optimizer/optimizer.h
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/optimizer/optimizer.h
@@ -17,25 +17,22 @@ namespace optimizer {
 
 class LocalizationOptimizer {
  public:
-  LocalizationOptimizer(
-      const kindr::minimal::QuatTransformation& architecture_offset,
-      const kindr::minimal::QuatTransformation& initial_pose,
-      const cad_percept::cgal::MeshModel::Ptr& arch_model,
-      const Eigen::Matrix<double, 6, 1>& architecture_offset_std,
-      const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
-      const double pointlaser_noise_std, const bool fix_retrieved_planes,
-      const bool add_prior, const bool only_optimize_translation);
-  Eigen::Vector3d addRelativeMeasurement(
-      const double distance,
-      const kindr::minimal::QuatTransformation joint2sensor);
+  LocalizationOptimizer(const kindr::minimal::QuatTransformation& architecture_offset,
+                        const kindr::minimal::QuatTransformation& initial_pose,
+                        const cad_percept::cgal::MeshModel::Ptr& arch_model,
+                        const Eigen::Matrix<double, 6, 1>& architecture_offset_std,
+                        const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
+                        const double pointlaser_noise_std, const bool fix_retrieved_planes,
+                        const bool add_prior, const bool only_optimize_translation);
+  Eigen::Vector3d addRelativeMeasurement(const double distance,
+                                         const kindr::minimal::QuatTransformation joint2sensor);
   void addOdometry(const kindr::minimal::QuatTransformation odometry);
   kindr::minimal::QuatTransformation optimize(const bool verbose = false);
 
  private:
   bool fix_retrieved_planes_, only_optimize_translation_;
   gtsam::ExpressionFactorGraph graph_;
-  kindr::minimal::QuatTransformation current_arm_pose_,
-      initial_architect_offset_;
+  kindr::minimal::QuatTransformation current_arm_pose_, initial_architect_offset_;
   ETransformation architect_offset_;
   cad_percept::cgal::MeshModel::Ptr architect_model_;
   gtsam::noiseModel::Base::shared_ptr pointlaser_noise_;
--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/optimizer/geometric_utils.cpp
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/optimizer/geometric_utils.cpp
@@ -8,8 +8,8 @@ namespace optimizer {
 
 Eigen::Matrix<double, 6, 1> getIntersectionPlaneImplementation(
     const kindr::minimal::QuatTransformation& sensor_pose,
-    const cad_percept::cgal::MeshModel::Ptr model,
-    gtsam::OptionalJacobian<6, 6> H, gtsam::OptionalJacobian<6, 1> H_ignored) {
+    const cad_percept::cgal::MeshModel::Ptr model, gtsam::OptionalJacobian<6, 6> H,
+    gtsam::OptionalJacobian<6, 1> H_ignored) {
   if (H) *H = Eigen::Matrix<double, 6, 6>::Zero();
   if (H_ignored) {
     // We know that H_ignored is never provided, because the function works only
@@ -17,40 +17,34 @@ Eigen::Matrix<double, 6, 1> getIntersectionPlaneImplementation(
     *H_ignored = Eigen::Matrix<double, 6, 1>::Zero();
   }
 
-  cad_percept::cgal::Ray query_ray =
-      cad_percept::cpt_utils::buildRayFromPose(sensor_pose);
+  cad_percept::cgal::Ray query_ray = cad_percept::cpt_utils::buildRayFromPose(sensor_pose);
   cad_percept::cgal::Intersection i = model->getIntersection(query_ray);
   Eigen::Matrix<double, 6, 1> response;
   response.head<3>() =
-      Eigen::Vector3d(i.intersected_point.x(), i.intersected_point.y(),
-                      i.intersected_point.z());
-  response.tail<3>() = Eigen::Vector3d(
-      i.surface_normal.x(), i.surface_normal.y(), i.surface_normal.z());
+      Eigen::Vector3d(i.intersected_point.x(), i.intersected_point.y(), i.intersected_point.z());
+  response.tail<3>() =
+      Eigen::Vector3d(i.surface_normal.x(), i.surface_normal.y(), i.surface_normal.z());
   return response;
 }
 
 gtsam::Expression<Eigen::Matrix<double, 6, 1>> getIntersectionPlane(
-    ETransformation& sensor_pose,
-    gtsam::Expression<cad_percept::cgal::MeshModel::Ptr>& model) {
-  return gtsam::Expression<Eigen::Matrix<double, 6, 1>>(
-      &getIntersectionPlaneImplementation, sensor_pose, model);
+    ETransformation& sensor_pose, gtsam::Expression<cad_percept::cgal::MeshModel::Ptr>& model) {
+  return gtsam::Expression<Eigen::Matrix<double, 6, 1>>(&getIntersectionPlaneImplementation,
+                                                        sensor_pose, model);
 }
 
 // Get the expected distance for the retrieved plane and the laser pose.
-gtsam::Expression<double> expectedDistance(ETransformation& laser_in_map,
-                                           EVector3& plane_support,
+gtsam::Expression<double> expectedDistance(ETransformation& laser_in_map, EVector3& plane_support,
                                            EVector3& plane_normal) {
   EVector3 unit_dir(Eigen::Vector3d(1, 0, 0));
   EVector3 origin(Eigen::Vector3d(0, 0, 0));
   // construct ray
-  EVector3 ray_direction =
-      rotate(rotationFromTransformation(laser_in_map), unit_dir);
+  EVector3 ray_direction = rotate(rotationFromTransformation(laser_in_map), unit_dir);
   EVector3 ray_origin = laser_in_map * origin;
   // find intersection distance
   gtsam::Expression<double> shortest_distance =
       multiplyVectors(plane_support - ray_origin, plane_normal);
-  return checkPositive(
-      divide(shortest_distance, multiplyVectors(ray_direction, plane_normal)));
+  return checkPositive(divide(shortest_distance, multiplyVectors(ray_direction, plane_normal)));
 }
 
 }  // namespace optimizer
