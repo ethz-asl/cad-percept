f047f91eb870756bfad65788c404084566818cb2
Laura <gasserl@mavt.ethz.ch>
2020-10-09 15:58:48 +0200

    mergefix

--- modules/localization/cpt_object_detection/src/object_detection.cpp
+++ modules/localization/cpt_object_detection/src/object_detection.cpp
@@ -472,6 +472,7 @@ void getDescriptors<LearnedDescriptor>(
   std::remove(filename_pointcloud.c_str());
   std::remove(filename_keypoints.c_str());
   std::remove(filename_descriptor.c_str());
+  return descriptors;
 }
 
 }  // namespace cad_percept::object_detection
