aa4ba0c72d0a428b01f49f4415f9c39d156e923f
Francesco Milano <francesco.milano172@gmail.com>
2020-10-12 09:39:58 +0200

    Style fixes in current version of localizer

--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/localizer/localizer.h
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/include/cpt_pointlaser_loc/localizer/localizer.h
@@ -14,12 +14,10 @@ class PointLaserLocalizer {
  public:
   /// \brief Construct a new PointLaserLocalizer object.
   ///
-  /// \param model                The mesh model w.r.t. which localization
-  ///                             should be performed.
+  /// \param model                The mesh model w.r.t. which localization should be performed.
   /// \param initial_pose_std     Standard deviation of the initial pose.
   /// \param odometry_noise_std   Standard deviation of the odometry.
-  /// TODO(fmilano): Check, the `odometry_noise_std` is not used internally by
-  ///               optimizer.
+  /// TODO(fmilano): Check, the `odometry_noise_std` is not used internally by optimizer.
   /// \param pointlaser_noise_std Standard deviation of the point-laser noise.
   PointLaserLocalizer(const cad_percept::cgal::MeshModel::Ptr& model,
                       const Eigen::Matrix<double, 6, 1>& initial_pose_std,
@@ -29,27 +27,18 @@ class PointLaserLocalizer {
   /// \brief Performs localization w.r.t. the mesh model.
   /// TODO(fmilano): Check for exact meaning of poses.
   ///
-  /// \param marker_to_armbase          Pose between the marker and the arm
-  ///                                   base.
-  /// \param initial_pose               Pose between the arm base and the Kinova
-  ///                                   link.
-  /// \param laser_a_offset             Pose between the Kinova link and the
-  ///                                   point laser A.
-  /// \param laser_b_offset             Pose between the Kinova link and the
-  ///                                   point laser B.
-  /// \param laser_c_offset             Pose between the Kinova link and the
-  ///                                   point laser C.
-  /// \param endeffector_offset         Pose between the Kinova link and the
-  ///                                   Kinova end effector.
+  /// \param marker_to_armbase          Pose between the marker and the arm base.
+  /// \param initial_pose               Pose between the arm base and the Kinova link.
+  /// \param laser_a_offset             Pose between the Kinova link and the point laser A.
+  /// \param laser_b_offset             Pose between the Kinova link and the point laser B.
+  /// \param laser_c_offset             Pose between the Kinova link and the point laser C.
+  /// \param endeffector_offset         Pose between the Kinova link and the Kinova end effector.
   /// \param arm_base_to_base           Pose between the arm base and the base.
-  /// \param fix_cad_planes             If True, the planes retrieved by the
-  ///                                   optimizer are fixed.
-  //  \param add_initial_pose_prior     If True, a factor for the prior of the
-  ///                                   model offset is added to the
-  ///                                   optimization graph.
+  /// \param fix_cad_planes             If True, the planes retrieved by the optimizer are fixed.
+  ///  \param add_initial_pose_prior    If True, a factor for the prior of the model offset is added
+  ///                                   to the optimization graph.
   /// \param only_optimize_translation  If True, only optimizes translation.
-  /// \return True if localization can be successfully performed, False
-  //          otherwise.
+  /// \return True if localization can be successfully performed, False otherwise.
   bool localize(const kindr::minimal::QuatTransformation& marker_to_armbase,
                 const kindr::minimal::QuatTransformation& initial_pose,
                 const kindr::minimal::QuatTransformation& laser_a_offset,
@@ -57,8 +46,7 @@ class PointLaserLocalizer {
                 const kindr::minimal::QuatTransformation& laser_c_offset,
                 const kindr::minimal::QuatTransformation& endeffector_offset,
                 const kindr::minimal::QuatTransformation& arm_base_to_base,
-                bool fix_cad_planes = false,
-                bool add_initial_pose_prior = false,
+                bool fix_cad_planes = false, bool add_initial_pose_prior = false,
                 bool only_optimize_translation = false);
 
  private:
--- modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/localizer/localizer.cpp
+++ modules/localization/cpt_pointlaser_loc/cpt_pointlaser_loc/src/cpt_pointlaser_loc/localizer/localizer.cpp
@@ -5,26 +5,24 @@
 namespace cad_percept {
 namespace pointlaser_loc {
 namespace localizer {
-PointLaserLocalizer::PointLaserLocalizer(
-    const cad_percept::cgal::MeshModel::Ptr& model,
-    const Eigen::Matrix<double, 6, 1>& initial_pose_std,
-    const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
-    double pointlaser_noise_std)
+PointLaserLocalizer::PointLaserLocalizer(const cad_percept::cgal::MeshModel::Ptr& model,
+                                         const Eigen::Matrix<double, 6, 1>& initial_pose_std,
+                                         const Eigen::Matrix<double, 6, 1>& odometry_noise_std,
+                                         double pointlaser_noise_std)
     : model_(model),
       initial_pose_std_(initial_pose_std),
       odometry_noise_std_(odometry_noise_std),
       pointlaser_noise_std_(pointlaser_noise_std) {}
 
-bool PointLaserLocalizer::localize(
-    const kindr::minimal::QuatTransformation& marker_to_armbase,
-    const kindr::minimal::QuatTransformation& initial_pose,
-    const kindr::minimal::QuatTransformation& laser_a_offset,
-    const kindr::minimal::QuatTransformation& laser_b_offset,
-    const kindr::minimal::QuatTransformation& laser_c_offset,
-    const kindr::minimal::QuatTransformation& endeffector_offset,
-    const kindr::minimal::QuatTransformation& arm_base_to_base,
-    bool fix_cad_planes, bool add_initial_pose_prior,
-    bool only_optimize_translation) {
+bool PointLaserLocalizer::localize(const kindr::minimal::QuatTransformation& marker_to_armbase,
+                                   const kindr::minimal::QuatTransformation& initial_pose,
+                                   const kindr::minimal::QuatTransformation& laser_a_offset,
+                                   const kindr::minimal::QuatTransformation& laser_b_offset,
+                                   const kindr::minimal::QuatTransformation& laser_c_offset,
+                                   const kindr::minimal::QuatTransformation& endeffector_offset,
+                                   const kindr::minimal::QuatTransformation& arm_base_to_base,
+                                   bool fix_cad_planes, bool add_initial_pose_prior,
+                                   bool only_optimize_translation) {
   // NOTE: we assume that the arm was already moved to the initial pose.
   // TODO(fmilano): Check!
 
